bb1796c0cdc1c4f027386bcb6a31325b208136b6
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-22 11:35:17 -0400

    fixed error checking on SAM keys that might not be present

--- vendor/management/commands/check_sam.py
+++ vendor/management/commands/check_sam.py
@@ -9,6 +9,15 @@ class Command(BaseCommand):
 
     logger = logging.getLogger('sam')
 
+
+    def get_value(self, obj, key, vendor):
+        try:
+            return obj[key]
+        except KeyError as k:
+            self.logger.debug("There was a key error on {0}: {1}".format(vendor.duns, k))
+            return None
+
+
     def handle(self, *args, **kwargs):
 
         vendors = Vendor.objects.all()
@@ -17,30 +26,31 @@ class Command(BaseCommand):
             uri = settings.SAM_API_URL + v.duns_4 + '?api_key=' + settings.SAM_API_KEY
             sam_data = requests.get(uri).json()
    
-            try:
-                if 'sam_data' in sam_data:
-                    if 'registration' in sam_data['sam_data']:
-                        reg = sam_data['sam_data']['registration']
-                        v.sam_status = reg['status']
-                        v.sam_exclusion = reg['hasKnownExclusion']
-                        v.oasis_address = reg['samAddress']['Line1']
-                        v.oasis_citystate = "{0}, {1} {2}".format(reg['samAddress']['City'], reg['samAddress']['stateorProvince'], reg['samAddress']['Zip'])
+            if 'sam_data' in sam_data:
+                if 'registration' in sam_data['sam_data']:
+                    reg = sam_data['sam_data']['registration']
+                    v.sam_status = self.get_value(reg, 'status', v)
+                    v.sam_exclusion = self.get_value(reg, 'hasKnownExclusion', v)
 
-                        v.sam_url = reg['corporateUrl']
-                        
+                    addr = self.get_value(reg, 'samAddress', v)
+                    if addr:
+                        v.oasis_address = self.get_value(addr, 'Line1', v)
+                        v.oasis_citystate = "{0}, {1} {2}".format(self.get_value(addr, 'City', v), 
+                                                                  self.get_value(addr, 'stateorProvince', v), 
+                                                                  self.get_value(addr, 'Zip', v))
+                
+                    v.sam_url = self.get_value(reg, 'corporateUrl', v)
 
-                        v.save()
+                    v.save()
 
-                        # fill out setasides
+                    # fill out setasides
 
-                    else:
-                        self.logger.debug("'registration' key is missing for {}".format(uri))
-
-                elif 'Error' in sam_data:
-                    self.logger.debug("SAM API returned an error for {0}, and duns {1}".format(uri, v.duns ))    
                 else:
-                    self.logger.debug("Could not load data from {} for unknown reason".format(uri))
+                    self.logger.debug("'registration' key is missing for {}".format(uri))
 
-            except KeyError as k:
-                self.logger.debug("There was a key error on {0}: {1}".format(v.duns, k))
+            elif 'Error' in sam_data:
+                self.logger.debug("SAM API returned an error for {0}, and duns {1}".format(uri, v.duns ))    
+            else:
+                self.logger.debug("Could not load data from {} for unknown reason".format(uri))
+
 
