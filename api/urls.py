49b1753cdfaf2a5646dc1c81c46e8d9d4bdb5c09
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-24 16:20:25 -0400

    class based views now and naics/setaside filtering

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
 
 urlpatterns = patterns('',
-    url(r'^vendors/$', 'api.views.vendor_list'),
+    url(r'^vendors/$', views.ListVendors.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
