0b782e05a1e78e3bac39a7ee0c1193d1fe9ad02c
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-24 14:18:31 -0400

    cleaner view function, format suffixes working

--- api/urls.py
+++ api/urls.py
@@ -4,3 +4,5 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = patterns('',
     url(r'^vendors/$', 'api.views.vendor_list'),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
+
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from vendor.models import Vendor
 from api.serializers import VendorSerializer
-from django.views.decorators.csrf import csrf_exempt
 
-class JSONResponse(HttpResponse):
-    
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-@csrf_exempt
-def vendor_list(request):
+@api_view(['GET',])
+def vendor_list(request, format=None):
     if request.method == 'GET':
         vendors = Vendor.objects.all()
         serializer = VendorSerializer(vendors, many=True)
-        return JSONResponse(serializer.data)
+        return  Response(serializer.data)
 
