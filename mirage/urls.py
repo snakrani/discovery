7c573f70c03bb216c61f1631734c52b5b4529386
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-25 14:58:03 -0400

    case insensitive naics search, working frontend with json dump

--- mirage/urls.py
+++ mirage/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+
 from api import urls as api_urls
 
 admin.autodiscover()
@@ -10,5 +14,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^api/', include(api_urls)),
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
