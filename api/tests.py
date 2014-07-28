83ecf717c552f5e447d10434644b33f736f75feb
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-28 17:15:08 -0400

    tests pass now, updated 404 test to check for empty list, added metadata to results for both endpoints

--- api/tests.py
+++ api/tests.py
@@ -33,6 +33,7 @@ class VendorsTest(TestCase):
         resp = self.c.get(self.path, {'format': 'json', 'naics': '541330'})
         self.assertEqual(resp.status_code, 200)
 
-    def test_request_invalid_naics_returns_404(self):
+    def test_request_invalid_naics_returns_empty_list(self):
         resp = self.c.get(self.path, {'format': 'json', 'naics': 'dlasfjosdf'})
-        self.assertEqual(resp.status_code, 404)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['num_results'], 0)
