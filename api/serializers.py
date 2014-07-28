59b9ad3535823c6165ec7740850c12484bad85d0
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-07-28 16:30:41 -0400

    added group parameter to vendors endpoint to allow grouping by pool

--- api/serializers.py
+++ api/serializers.py
@@ -14,18 +14,32 @@ class NaicsSerializer(serializers.ModelSerializer):
 
 class PoolSerializer(serializers.ModelSerializer):
     naics = NaicsSerializer(many=True)
-
     class Meta:
         model = Pool
         fields = ('id', 'number', 'vehicle', 'naics', 'threshold')
 
+class ShortPoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pool
+        fields = ('id', 'number', 'vehicle')
+
+
 class VendorSerializer(serializers.ModelSerializer):
     setasides = SetAsideSerializer(many=True)
-    pools = PoolSerializer(many=True)
+    pools = ShortPoolSerializer(many=True)
 
     class Meta:
         model = Vendor
         fields = ('name', 'duns', 'duns_4', 'sam_address', 'sam_citystate', 'pools', 'setasides', 'sam_status', 'sam_exclusion', 'sam_url')
 
-    
+class ShortVendorSerializer(serializers.ModelSerializer):
+    setasides = SetAsideSerializer(many=True)
+
+    class Meta:
+        model = Vendor
+        fields = ('name', 'duns', 'duns_4', 'sam_address', 'sam_citystate', 'setasides', 'sam_status', 'sam_exclusion', 'sam_url')
+
+
+
+   
 
