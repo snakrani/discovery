5b6053a9173e3ad535ffa72470f4374c2648f95a
Joshua Ruihley <ruihley@gmail.com>
2014-07-28 15:59:29 -0400

    - added tests for naics and vendor endpoints - moved api fixtures to fixtures dir - removed logs from repo - added DS_Store to gitignore

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+class NaicsTest(TestCase):
+    """tests for NAICS API endpoint"""
+    fixtures = ['naics.json']
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_request_no_params(self):
+        resp = self.c.get('/api/naics/', {'format': 'json'})
+        self.assertEqual(resp.status_code, 200)
+
+    def test_request_q_param(self):
+        resp = self.c.get('/api/naics/', {'q': 'test'})
+        self.assertEqual(resp.status_code, 200)
+
+
+class VendorsTest(TestCase):
+    """test for vendor API endpoint"""
+    fixtures = ['vendors.json']
+ 
+    def setUp(self):
+        self.c = Client()
+        self.path = '/api/vendors/'
+
+    def test_request_no_params(self):
+        resp = self.c.get(self.path, {'format': 'json'})
+        self.assertEqual(resp.status_code, 200)
+
+    def test_request_valid_naics(self):
+        resp = self.c.get(self.path, {'format': 'json', 'naics': '541330'})
+        self.assertEqual(resp.status_code, 200)
+
+    def test_request_invalid_naics(self):
+        resp = self.c.get(self.path, {'format': 'json', 'naics': 'dlasfjosdf'})
+        self.assertEqual(resp.status_code, 404)
