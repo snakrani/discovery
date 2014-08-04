3441ed47f2c95d51b3d50c03f6b296a1e88f9d14
Joshua Ruihley <ruihley@gmail.com>
2014-08-04 17:51:37 -0400

    added vagrant configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,17 @@ apt-get install -y python-virtualenv
 apt-get install -y libpq-dev python-dev
 
 #set up database
-echo "CREATE ROLE oasis LOGIN password 'secret'; CREATE DATABASE oasis ENCODING 'UTF8' OWNER oasis;" | sudo -u postgres psql
-/etc/init.d/postgresql restart
+echo "CREATE USER oasis WITH password 'oasis'; CREATE DATABASE oasis ENCODING 'UTF8' OWNER oasis;" | sudo -u postgres psql
+service postgresql restart
 
-#set up application
+#set up virtual environment
 virtualenv /vagrant/mirage_venv
 source /vagrant/mirage_venv/bin/activate
 pip install -r /vagrant/requirements.txt
+
+#set up application
+source /vagrant/mirage_venv/bin/activate
+/vagrant/manage.py syncdb --noinput
+/vagrant/manage.py loaddata /vagrant/vendor/fixtures/naics.json
+/vagrant/manage.py load_vendors
+/vagrant/manage.py runserver 0.0.0.0:8000
