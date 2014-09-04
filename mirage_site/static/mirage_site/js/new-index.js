6dee9f363142cc69da471c3a63684ea5c80db8f2
Theresa Summa <theresa.summa@gsa.gov>
2014-09-04 23:44:11 +0000

    implemented url manager

--- mirage_site/static/mirage_site/js/new-index.js
+++ mirage_site/static/mirage_site/js/new-index.js
@@ -40,7 +40,7 @@ var ResultsManager = {
         Events.subscribe('filtersChanged', this.load.bind(ResultsManager));
     },
 
-    buildRequestQS: function() {
+    buildRequestQuery: function() {
         var setasides = InputHandler.getSetasides();
         var naicsCode = InputHandler.getNAICSCode();
         var queryData = {
@@ -61,10 +61,13 @@ var ResultsManager = {
 
     load: function() {
         var url = "/api/vendors/";
-        var queryData = this.buildRequestQS();        
+        var queryData = this.buildRequestQuery();        
 
         $.getJSON(url, queryData, function(data) {
             console.log(data);
+            ResultsManager.vehicle = data['results'][0]['vehicle'].toLowerCase();
+            ResultsManager.poolNumber = data['results'][0]['number'];
+            Events.publish('dataLoaded');
         });
     },
 
@@ -72,6 +75,50 @@ var ResultsManager = {
         return null;
     },
 
+    getVehicle: function() {
+        return this.vehicle;
+    },
+
+    getPoolNumber: function() {
+        return this.poolNumber;
+    }
+
+};
+
+var URLManager = {
+    init: function() {
+        Events.subscribe('contentChanged', this.update.bind(URLManager));
+    },
+
+    getQueryString: function() {
+        var queryObject = ResultsManager.buildRequestQuery();
+        var qs = '?';
+        var k;
+
+        for (k in queryObject) {
+            qs += k + '=' + queryObject[k] + '&';
+        }
+
+        return qs;
+    },
+
+    update: function() {
+        var qs = this.getQueryString();
+        var vehicle = ResultsManager.getVehicle();
+        var poolNumber = ResultsManager.getPoolNumber();
+
+        window.location.href = '/pool/' + vehicle + '/' + poolNumber + '/' + qs;
+    }
+};
+
+var LayoutManager = {
+    init: function() {
+        Events.subscribe('dataLoaded', this.render.bind(LayoutManager));
+    },
+
+    render: function() {
+        Events.publish('contentChanged');
+    }
 };
 
 $(document).ready(function() {
@@ -80,4 +127,6 @@ $(document).ready(function() {
 
     InputHandler.init();
     ResultsManager.init();
+    URLManager.init();
+    LayoutManager.init();
 });
