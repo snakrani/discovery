6538c9da5fdcd883ef8b17c49addf1b6f9a0711c
Joshua Ruihley <ruihley@gmail.com>
2014-09-08 11:00:25 -0400

    moved secret key to local_settings

--- mirage/local_settings.example.py
+++ mirage/local_settings.example.py
@@ -8,3 +8,7 @@ DATABASES = {
 }
 
 SAM_API_KEY = ''
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# change this before deploying to your own server!
+SECRET_KEY = '&%7l9-fvi7_9ykzb*kr1bhjfx%x=(vd0r8z4w#p154eb1o+t=o'
