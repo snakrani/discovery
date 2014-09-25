ceb47493ca044bf92ec5b8c7c96f5b303197c043
Joshua Ruihley <ruihley@gmail.com>
2014-09-25 14:18:51 -0400

    - added title and pool number to pool results - added csv link

--- mirage_site/static/mirage_site/js/layout-manager.js
+++ mirage_site/static/mirage_site/js/layout-manager.js
@@ -35,6 +35,13 @@ var LayoutManager = {
 
         $(".results_pool_name_number_pool").text("Pool " + results.results[0]['number'] + ": ");
         $(".results_pool_name_number_description").text(results.results[0]['name']);
+
+        //generate csv link (sloppy)
+        var pathArray = window.location.href.split('/');
+        pathArray.splice(4, 0, "csv");
+        console.log(pathArray.join('/'));
+        $("#csv_link").attr("href", pathArray.join('/'));
+        
         $("#number_of_results span").text(resultsStr);
         $("#your_search").text($("#naics-code option:selected").text());
         $("#your_filters").text(
@@ -49,5 +56,9 @@ var LayoutManager = {
     toTitleCase: function(str) {
         // from http://stackoverflow.com/questions/5097875/help-parsing-string-city-state-zip-with-javascript
         return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    },
+
+    urlInsertCSV: function() {
+
     }
 };
