b57746d60d8ef212f539eb8cfb101a23314c86b8
Kaitlin Devine <kaitlin.devine@gsa.gov>
2014-10-21 11:13:36 -0400

    closes #198. added click event in url-manager to handle routing for back to search results

--- mirage_site/static/mirage_site/js/url-manager.js
+++ mirage_site/static/mirage_site/js/url-manager.js
@@ -17,6 +17,7 @@ var URLManager = {
         Events.subscribe('contentChanged', this.update.bind(URLManager));
         Events.subscribe('vehicleChanged', this.update.bind(URLManager));
         Events.subscribe('goToPoolPage', this.loadPoolPage.bind(URLManager));
+        $('#combined_search_pool').on('click', 'a', function() { window.location = "/results/" + window.location.search.replace("#", ""); }); 
     },
 
     getQueryString: function() {
