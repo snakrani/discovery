53cfb9dad221c8b7280fd8c9a6a04466937daa21
Adrian Webb <adrian.webb@coraltech.net>
2017-12-06 02:16:10 -0500

    Adding PostgreSQL external access configurations in the bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-#apt-get update -y
 apt-get install -y git
 apt-get install -y postgresql-9.3 pgadmin3
 apt-get install -y python-pip
@@ -8,6 +7,9 @@ apt-get install -y python-virtualenv
 apt-get install -y libpq-dev python-dev
 
 #set up database
+sed -i 's/^#listen_addresses.*/listen_addresses = '"'"'*'"'"'/' /etc/postgresql/9.3/main/postgresql.conf
+grep -q "0.0.0.0/0" /etc/postgresql/9.3/main/pg_hba.conf || echo "host  all  all  0.0.0.0/0  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+
 echo "CREATE USER oasis WITH password 'oasis'; CREATE DATABASE oasis ENCODING 'UTF8' OWNER oasis;" | sudo -u postgres psql
 service postgresql restart
 
