ed35e91ce36a7cc66ef003a4f740395139e48dc2
Adrian Webb <adrian.webb@coraltech.net>
2017-12-14 13:04:58 -0500

    Adding an update FPDS celery task in the contract app.

--- contract/tasks.py
+++ contract/tasks.py
@@ -1,3 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
+from django.core.management import call_command
+
+
+@shared_task
+def update_fpds(years=10, weeks=520, count=500, pause=1):
+    call_command('load_fpds', 
+                 years=years, 
+                 weeks=weeks, 
+                 count=count, 
+                 pause=pause
+    )
