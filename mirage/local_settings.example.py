f82f9d26e4357ebce314f45fefafdc022680dcf0
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-17 19:11:45 -0500

    Adding a celery scheduler configuration in the local settings example.

--- mirage/local_settings.example.py
+++ mirage/local_settings.example.py
@@ -19,7 +19,7 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
-CELERY_BEAT_SCHEDULE = {}
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 SAUCE = False
 SAUCE_USERNAME = ''
