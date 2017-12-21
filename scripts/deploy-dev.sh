84a739d687448f7657fb18ffa64e43d8838af6ba
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-21 05:28:53 -0500

    Adding an initial development deploy script.

---
+++ scripts/deploy-dev.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Deploy the GSA PSHC discovery application development infrastructure.
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+cd "$SCRIPT_DIR/.."
+
+# Deploy...
+
+cf push -f manifest-develop.yml
