183114c020e1b383a66eab7a77c5dd2f2c0b17db
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-20 21:32:23 -0500

    Updating the stdout capture and return values of the update_fpds and update_vendors Celery tasks.

--- contract/tasks.py
+++ contract/tasks.py
@@ -2,13 +2,33 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
 from django.core.management import call_command
+from StringIO import StringIO
+
+import sys
 
 
 @shared_task
 def update_fpds(years=10, weeks=520, count=500, pause=1):
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    
+    #TODO: Return some data?
     call_command('load_fpds', 
                  years=years, 
                  weeks=weeks, 
                  count=count, 
                  pause=pause
     )
+    
+    sys.stdout = old_stdout
+        
+    return { 
+        "task": "update_fpds",
+        "params": { 
+                   "years": years, 
+                   "weeks": weeks,
+                   "count": count, 
+                   "pause": pause
+        },
+        "message": mystdout.getvalue() 
+    }
