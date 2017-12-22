58b7116f57ca11b248db0a82bce9d4259a9475b8
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-22 11:39:17 -0500

    Updating the bootstrap script to use other scripts.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,88 +5,49 @@ PROJ_DIR="${1:-/vagrant}"
 
 #-------------------------------------------------------------------------------
 
-#install dependencies
-apt-get update
-apt-get install -y git
-apt-get install -y postgresql-9.3 pgadmin3
-apt-get install -y python-pip
-apt-get install -y python-virtualenv
-apt-get install -y libpq-dev python-dev
-apt-get install -y fontconfig wget
-
-#set up database
-if [ ! -f /tmp/postgresql ]
+#install basic dependencies
+if [ ! -f /tmp/apt-update-complete ]
 then
-  sed -i 's/^#listen_addresses.*/listen_addresses = '"'"'*'"'"'/' /etc/postgresql/9.3/main/postgresql.conf
-  grep -q "0.0.0.0/0" /etc/postgresql/9.3/main/pg_hba.conf || echo "host  all  all  0.0.0.0/0  md5" >> /etc/postgresql/9.3/main/pg_hba.conf
-  touch /tmp/postgresql
+  sudo apt-get update
+  touch /tmp/apt-update-complete 
 fi
-echo "CREATE USER oasis WITH password 'oasis'; ALTER USER oasis CREATEDB; CREATE DATABASE oasis ENCODING 'UTF8' OWNER oasis;" | sudo -u postgres psql
+sudo apt-get install -y git
 
-#set up redis queue
-if [ ! -f /etc/init.d/redis ]
-then
-  cd /tmp
-  wget http://download.redis.io/redis-stable.tar.gz
-  tar xvzf redis-stable.tar.gz
-  cd redis-stable
-  make
-  make install
+#install CloudFoundry CLI
+"$PROJ_DIR/scripts/setup-cf.sh"
 
-  mkdir -p /etc/redis
-  cp -f redis.conf /etc/redis/6379.conf # binds to 127.0.0.1:6379
+#set up PostgreSQL database
+"$PROJ_DIR/scripts/setup-postgresql.sh"
 
-  sed -i 's/^daemonize\s.*/daemonize yes/' /etc/redis/6379.conf
-  sed -i 's/^dir\s.*/dir "\/var\/redis\/6379"/' /etc/redis/6379.conf
-  sed -i 's/^logfile\s.*/logfile "\/var\/log\/redis_6379.log"/' /etc/redis/6379.conf
-  sed -i 's/^loglevel\s.*/loglevel debug/' /etc/redis/6379.conf
+#set up Redis queue
+"$PROJ_DIR/scripts/setup-redis.sh"
 
-  mkdir -p /var/redis/6379
+#install PhantomJS
+"$PROJ_DIR/scripts/setup-phantomjs.sh"
 
-  cp -f utils/redis_init_script /etc/init.d/redis # listens at 6379
-  update-rc.d redis defaults
-fi
-
-#set up virtual environment
-if [ ! -d "$PROJ_DIR/venv" ]
-then
-  virtualenv "$PROJ_DIR/venv"
-fi
+#set up Python
+"$PROJ_DIR/scripts/setup-python.sh"
 source "$PROJ_DIR/venv/bin/activate"
-pip install -r "$PROJ_DIR/requirements.txt"
-pip install -r "$PROJ_DIR/requirements-test.txt"
 
-#install phantomJS
-if [ ! -f "$PROJ_DIR/venv/bin/phantomjs" ]
-then
-  wget -O /tmp/phantomjs.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-  tar -xjf /tmp/phantomjs.tar.bz2 -C /tmp
-  mv /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs "$PROJ_DIR/venv/bin/phantomjs"
-fi
-
-#set up application
+#set up Django application
 "$PROJ_DIR/manage.py" migrate --noinput
 "$PROJ_DIR/manage.py" createcachetable
 "$PROJ_DIR/manage.py" collectstatic --noinput
 
-#create admin user
-echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | "$PROJ_DIR/manage.py" shell
-
 #load starter data
 "$PROJ_DIR/scripts/load-fixtures.sh"
 
+#create admin user
+"$PROJ_DIR/scripts/create-admin.sh" admin admin
+
 #setup background processes
-cp -f "$PROJ_DIR/scripts/bootstrap/celery-vars.sh" /etc/default/celery
-cp -f "$PROJ_DIR/scripts/bootstrap/celery-init.sh" /etc/init.d/celery
-cp -f "$PROJ_DIR/scripts/bootstrap/celerybeat-vars.sh" /etc/default/celerybeat
-cp -f "$PROJ_DIR/scripts/bootstrap/celerybeat-init.sh" /etc/init.d/celerybeat
+sudo cp -f "$PROJ_DIR/scripts/celery/celery-vars.sh" /etc/default/celery
+sudo cp -f "$PROJ_DIR/scripts/celery/celery-init.sh" /etc/init.d/celery
+sudo cp -f "$PROJ_DIR/scripts/celery/celerybeat-vars.sh" /etc/default/celerybeat
+sudo cp -f "$PROJ_DIR/scripts/celery/celerybeat-init.sh" /etc/init.d/celerybeat
 
 #run applications and services
-/etc/init.d/postgresql restart
-
-/etc/init.d/redis stop
-/etc/init.d/redis start
-
 "$PROJ_DIR/manage.py" runserver "0.0.0.0:8000" &
-/etc/init.d/celery restart
-/etc/init.d/celerybeat restart
+
+sudo /etc/init.d/celery restart
+sudo /etc/init.d/celerybeat restart
