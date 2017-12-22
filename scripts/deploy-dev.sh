f91dd1da7a6317cca5193ef401507ce3e5f5eb40
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-22 16:01:23 -0500

    Implementing the deploy_dev script.

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -4,6 +4,29 @@
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 cd "$SCRIPT_DIR/.."
 
-# Deploy...
+set -e # Bomb if anything fails
 
-cf push -f manifest-develop.yml
+# Deployment goodness
+#
+# > CloudFoundry (cloud.gov) with Autopilot and Antifreeze plugins
+
+cf_login() {
+  cf login -a "$LOGIN_URL" -u "$DEV_SERVICE_ACCOUNT" -p "$DEV_SERVICE_ACCOUNT_PASSWORD" -o "$DEV_SERVICE_ORG" -s "$DEV_SERVICE_SPACE"  
+}
+
+check_app_env() {
+  cf check-manifest discovery-test -f manifest-develop.yml
+  #cf check-manifest discovery-celerybeat -f manifest-develop.yml
+  #cf check-manifest discovery-celery -f manifest-develop.yml
+}
+
+deploy_app() {
+  cf zero-downtime-push discovery-test -f manifest-develop.yml
+  #cf push discovery-celerybeat -f manifest-develop.yml
+  #cf push discovery-celery -f manifest-develop.yml
+}
+
+# Check and deploy
+cf_login
+check_app_env
+deploy_app
