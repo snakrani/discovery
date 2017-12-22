8a6a391a3f46c851869861772b71a054424875f4
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-22 11:37:13 -0500

    Adding a setup-phantomjs script.

---
+++ scripts/setup-phantomjs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Setup PhantomJS.
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+cd "$SCRIPT_DIR/.."
+
+#download and install PhantomJS if it does not exist
+if [ ! -f venv/bin/phantomjs ]
+then
+  if [ ! -f /tmp/apt-update-complete ]
+  then
+    sudo apt-get update
+    touch /tmp/apt-update-complete 
+  fi
+  sudo apt-get install -y fontconfig wget
+  
+  wget -O /tmp/phantomjs.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+  tar -xjf /tmp/phantomjs.tar.bz2 -C /tmp
+  mv /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs venv/bin/phantomjs
+fi
