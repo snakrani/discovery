510298e7361e8e4fadd6ea723513fd0f3989a94f
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-24 06:40:23 -0500

    Refactoring the bootstrap script to install and use Git, CF, and Docker Compose.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,9 +2,7 @@
 #-------------------------------------------------------------------------------
 
 PROJ_DIR="${1}" # Required!!
-ADMIN_USER="${2:-admin}"
-ADMIN_PASSWORD="${3:-admin}"
-ADMIN_EMAIL="${4:-admin@example.com}"
+cd "$PROJ_DIR"
 
 #-------------------------------------------------------------------------------
 
@@ -12,50 +10,21 @@ ADMIN_EMAIL="${4:-admin@example.com}"
 if [ ! -f /tmp/apt-update-complete ]
 then
   echo "> Updating OS package repositories"
-  sudo apt-get update > /dev/null
+  sudo apt-get update >/dev/null
   touch /tmp/apt-update-complete 
 fi
-if ! which git > /dev/null
+if ! which git >/dev/null
 then
   echo "> Installing Git version control"
-  sudo apt-get install -y git > /dev/null 2>&1
+  sudo apt-get install -y git >/dev/null 2>&1
 fi
 
 #install CloudFoundry CLI
-"$PROJ_DIR/scripts/setup-cf.sh"
+./scripts/setup-cf.sh
 
-#set up PostgreSQL database
-"$PROJ_DIR/scripts/setup-postgresql.sh"
+#install Docker and Docker Compose
+./scripts/setup-docker.sh
 
-#set up Redis queue
-"$PROJ_DIR/scripts/setup-redis.sh"
-
-#install PhantomJS
-"$PROJ_DIR/scripts/setup-phantomjs.sh"
-
-#set up Python
-"$PROJ_DIR/scripts/setup-python.sh"
-
-#set up Django application
-"$PROJ_DIR/scripts/init-server.sh"
-
-#create admin user
-"$PROJ_DIR/scripts/create-admin.sh" "$ADMIN_USER" "$ADMIN_PASSWORD" "$ADMIN_EMAIL"
-
-#setup background processes
-echo "> Copying Celery service configurations and scripts"
-sudo cp -f "$PROJ_DIR/scripts/celery/celery-vars.sh" /etc/default/celery
-sudo cp -f "$PROJ_DIR/scripts/celery/celery-init.sh" /etc/init.d/celery
-sudo cp -f "$PROJ_DIR/scripts/celery/celerybeat-vars.sh" /etc/default/celerybeat
-sudo cp -f "$PROJ_DIR/scripts/celery/celerybeat-init.sh" /etc/init.d/celerybeat
-
-#run applications and services
-echo "> Starting the Django web application"
-source "$PROJ_DIR/venv/bin/activate"
-"$PROJ_DIR/manage.py" runserver "0.0.0.0:8000" &
-
-echo "> Starting the Celery worker"
-sudo /etc/init.d/celery restart > /dev/null
-  
-echo "> Starting the Celery scheduler"
-sudo /etc/init.d/celerybeat restart > /dev/null
+#run Docker applications
+echo "> Running all Docker services"
+docker-compose up -d
