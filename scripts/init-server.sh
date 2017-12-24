cd0e91021d1786c706c84902fd4dca72db37a226
Adrian Webb <adrianwebb.78@gmail.com>
2017-12-24 06:49:17 -0500

    Updates to the init-server script.

--- scripts/init-server.sh
+++ scripts/init-server.sh
@@ -1,22 +1,49 @@
 #!/usr/bin/env bash
 # Prepare a python enabled server for application hosting.
 
+set -e
+
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 cd "$SCRIPT_DIR/.."
 
+LOG_FILE="${1:-./logs/discovery-init.log}"
+if [ "$LOG_FILE" != "/dev/stdout" -a "$LOG_FILE" != "/dev/stderr" ]
+then
+  rm -f "$LOG_FILE"
+fi
+
 #activate the virtual python environment
-source venv/bin/activate
+if [ -d /venv ]
+then
+  alias python="/venv/bin/python"
+  source /venv/bin/activate
+fi
+
+DB_HOST="${2:-none}"
+DB_PORT="${3:-5432}"
+
+if which git >/dev/null
+then
+  git submodule update --init --recursive
+fi
+if [ "$DB_HOST" != "none" ]
+then
+  ./scripts/wait-for-it/wait-for-it.sh --host="$DB_HOST" --port="$DB_PORT"
+fi
 
 #run application setup commands
 
-echo "> Migrating Django database structure"
-./manage.py migrate --noinput > /dev/null
+echo "> Migrating Django database structure" | tee -a "$LOG_FILE"
+python manage.py migrate --noinput >>"$LOG_FILE" 2>&1
 
-echo "> Ensuring Django cache table"
-./manage.py createcachetable > /dev/null
+echo "> Ensuring Django cache table" | tee -a "$LOG_FILE"
+python manage.py createcachetable >>"$LOG_FILE" 2>&1
 
-echo "> Collecting Django static files"
-./manage.py collectstatic --noinput > /dev/null
+echo "> Collecting Django static files" | tee -a "$LOG_FILE"
+python manage.py collectstatic --noinput >>"$LOG_FILE" 2>&1
 
-echo "> Loading basic category information"
-./manage.py load_categories > /dev/null
+echo "> Loading basic category information" | tee -a "$LOG_FILE"
+python manage.py load_categories >>"$LOG_FILE" 2>&1
+
+# Create admin user ONLY IF it doesn't exist yet
+./scripts/create-admin.sh admin admin-changeme >>"$LOG_FILE" 2>&1
