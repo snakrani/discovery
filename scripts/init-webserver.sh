5fef801b965ba4948cfd7f236e146e6ee63b0458
Adrian Webb <adrianwebb.78@gmail.com>
2018-01-04 15:29:28 -0500

    Splitting the init-server script into init-webserver and init-db scripts.

---
+++ scripts/init-webserver.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Prepare a python enabled webserver for application hosting.
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+cd "$SCRIPT_DIR/.."
+
+LOG_FILE="${1:-./logs/discovery-init.log}"
+if [ "$LOG_FILE" != "/dev/stdout" -a "$LOG_FILE" != "/dev/stderr" ]
+then
+  rm -f "$LOG_FILE"
+fi
+
+#activate the virtual python environment
+if [ -d /venv ]
+then
+  alias python="/venv/bin/python"
+  source /venv/bin/activate
+fi
+
+#run application setup commands
+
+echo "> Collecting Django static files" | tee -a "$LOG_FILE"
+python manage.py collectstatic --noinput >>"$LOG_FILE" 2>&1
