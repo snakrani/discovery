1cfaec4474c50f443d318af0704e5658273ee6e0
Adrian Webb <adrianwebb.78@gmail.com>
2018-01-11 17:00:43 -0500

    Adding an initial create-security-docs script.

---
+++ scripts/create-security-docs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Generate security documentation for the GSA PSHC discovery application.
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+cd "$SCRIPT_DIR/.."
+
+# Fetch Compliance dependencies into opencontrols directory
+compliance-masonry get
+
+# Generate security documentation
+compliance-masonry docs gitbook --markdowns compliance/markdowns --exports opencontrols/exports
