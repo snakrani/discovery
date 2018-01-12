f8b7b48fb36f1e650fc56c9f6332602235aa9732
Adrian Webb <adrianwebb.78@gmail.com>
2018-01-11 23:46:44 -0500

    Adding PDF generation to the create-security-docs script.

--- scripts/create-security-docs.sh
+++ scripts/create-security-docs.sh
@@ -4,11 +4,20 @@
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 cd "$SCRIPT_DIR/.."
 
+CONTROL_PDF_FILE="${1}"
+EXPORT_DIR="opencontrols/export"
+
 # Remove old security documentation
 rm -Rf opencontrols
 
-echo "Fetching compliance definition dependencies into opencontrols directory"
+echo "> Fetching compliance definition dependencies into opencontrols directory"
 compliance-masonry get
 
-echo "Generating security documentation"
-compliance-masonry docs gitbook --markdowns compliance/gitbook/markdown --exports opencontrols/export LATO
+echo "> Generating security documentation"
+compliance-masonry docs gitbook --markdowns compliance/gitbook/markdown --exports "$EXPORT_DIR" LATO
+
+if [ ! -z "$CONTROL_PDF_FILE" ]
+then
+  echo "> Generating PDF document from security documentation"
+  gitbook pdf "$EXPORT_DIR" "$CONTROL_PDF_FILE"
+fi
