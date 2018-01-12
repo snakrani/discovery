dcd5304d1ad3ce4c917441a900b4cf21419367a1
Adrian Webb <adrianwebb.78@gmail.com>
2018-01-11 23:11:13 -0500

    Moving the gitbook markdown directory and adjusting the create-security-docs script.

--- scripts/create-security-docs.sh
+++ scripts/create-security-docs.sh
@@ -4,8 +4,11 @@
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 cd "$SCRIPT_DIR/.."
 
+# Remove old security documentation
+rm -Rf opencontrols
+
 echo "Fetching compliance definition dependencies into opencontrols directory"
 compliance-masonry get
 
 echo "Generating security documentation"
-compliance-masonry docs gitbook --markdowns compliance/markdown --exports opencontrols/export LATO
+compliance-masonry docs gitbook --markdowns compliance/gitbook/markdown --exports opencontrols/export LATO
