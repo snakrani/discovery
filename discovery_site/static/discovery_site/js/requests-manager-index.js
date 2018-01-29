aa1e9afa7c984ed47a9cafce1435cc580399400e
Adrian Webb <adrianwebb.78@gmail.com>
2018-01-29 00:57:42 +0000

    Updating the initialization method in the requests manager index js.

--- discovery_site/static/discovery_site/js/requests-manager-index.js
+++ discovery_site/static/discovery_site/js/requests-manager-index.js
@@ -1,22 +1,12 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-'use strict';
- 
-RequestsManager.indexInit = function(original) {
-    original.bind(RequestsManager).call();
-    
+
+RequestsManager.initializers.index = function() {
     this.loadMetadata();
 };
- 
-RequestsManager.originalInit = RequestsManager.init;
- 
-RequestsManager.init = function() {
-    RequestsManager.indexInit(RequestsManager.originalInit);
-};
- 
+
 RequestsManager.loadMetadata = function() {
     var url = "/api/metadata/";
     RequestsManager.getAPIRequest(url, {}, function(data) {
-        var resultsObj = data; 
+        var resultsObj = data;
         Events.publish('metaDataLoaded', resultsObj);
     });
 };
