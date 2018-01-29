dc386be9142260670bedf32ec2c1904d0d66e4dd
Adrian Webb <adrianwebb.78@gmail.com>
2018-01-29 01:00:12 +0000

    Refactoring the requests manager listings js.

--- discovery_site/static/discovery_site/js/requests-manager-listings.js
+++ discovery_site/static/discovery_site/js/requests-manager-listings.js
@@ -1,30 +1,48 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-'use strict';
 
-// for pool and vendor lists pages
-// anything under url /pool
-RequestsManager.load = function() {
+RequestsManager.vendorsPageCount = 25;
+
+RequestsManager.initializers.listings = function() {
+    Events.subscribe('vendorsChanged', this.refreshVendors.bind(RequestsManager));
+};
+
+RequestsManager.loadVendors = function(data, callback) {
     var url = "/api/vendors/";
-    var queryData = this.buildRequestQuery();
-   
+    var queryData = $.extend(data, this.buildRequestQuery(), {'count': RequestsManager.vendorsPageCount});
+
     if (queryData['naics'] !== "") {
+        RequestsManager.getAPIRequest(url, queryData, function(response) {
+            var resultsObj = {};
 
-        RequestsManager.getAPIRequest(url, queryData, function(data) {
-            var resultsObj = {}; 
-
-            resultsObj.poolNumber = data['pool'][0]['number'];
-            resultsObj.poolName = data['pool'][0]['name'];
-            resultsObj.samLoad = data.sam_load;
+            resultsObj.poolNumber = response['pools'][0]['number'];
+            resultsObj.poolName = response['pools'][0]['name'];
+            resultsObj.lastUpdated = response['last_updated'];
             resultsObj.total = 0; //overwritten below if there are any
 
-            if (data.num_results !== 0) {
-                resultsObj.vehicle = data['pool'][0]['vehicle'].toLowerCase();
-                resultsObj.numPools = data.results.length;
-                resultsObj.total = data.num_results;
-                resultsObj.results = data.results;
-                resultsObj['naics-code'] = queryData['naics-code'];
-            }         
-            Events.publish('dataLoaded', resultsObj);
+            if (response['num_results'] !== 0) {
+                resultsObj.naics = queryData['naics'];
+                resultsObj.vehicle = response['pools'][0]['vehicle'].toLowerCase();
+                resultsObj.total = response['num_results'];
+                resultsObj.count = response['page']['results'].length;
+                resultsObj.results = response['page']['results'];
+            }
+
+            callback(queryData, response, resultsObj);
         });
     };
+};
+
+RequestsManager.load = function() {
+    RequestsManager.loadVendors(LayoutManager.currentSortParams(), function(queryData, response, results) {
+        Events.publish('dataLoaded', results);
+    });
+};
+
+RequestsManager.refreshVendors = function(data) {
+    if (data['sort'] && !data['direction']) {
+        data['direction'] = 'desc';
+    }
+
+    RequestsManager.loadVendors(data, function(queryData, response, results) {
+        Events.publish('vendorDataLoaded', results, data['page'], RequestsManager.vendorsPageCount);
+    });
 };
