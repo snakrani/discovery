b34b2d9562db1780803324bb058707a9222b5fc9
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-05 22:11:35 +0000

    Adding locations and place of performance models to the generate and load fixtures scripts.

--- scripts/gen-fixtures.sh
+++ scripts/gen-fixtures.sh
@@ -9,11 +9,13 @@ cd "$SCRIPT_DIR/.."
 ./manage.py dumpdata vendors.naics --indent=2 > vendors/fixtures/naics.json
 ./manage.py dumpdata vendors.setaside --indent=2 > vendors/fixtures/setasides.json
 ./manage.py dumpdata vendors.pool --indent=2 > vendors/fixtures/pools.json # reqs: naics
-./manage.py dumpdata vendors.vendor --indent=2 > vendors/fixtures/vendors.json # reqs: setasides
+./manage.py dumpdata vendors.location --indent=2 > vendors/fixtures/locations.json
+./manage.py dumpdata vendors.vendor --indent=2 > vendors/fixtures/vendors.json # reqs: locations, setasides
 ./manage.py dumpdata vendors.poolpiid --indent=2 > vendors/fixtures/poolpiids.json # reqs: pool, vendor
 ./manage.py dumpdata vendors.samload --indent=2 > vendors/fixtures/samloads.json
 
 # Generate contract related fixtures
 
+./manage.py dumpdata contract.placeofperformance --indent=2 > contract/fixtures/placesofperformace.json
 ./manage.py dumpdata contract.contract --indent=2 > contract/fixtures/contracts.json #reqs: naics, vendor, piid
 ./manage.py dumpdata contract.fpdsload --indent=2 > contract/fixtures/fpdsloads.json #reqs: vendor
--- scripts/load-fixtures.sh
+++ scripts/load-fixtures.sh
@@ -9,11 +9,13 @@ cd "$SCRIPT_DIR/.."
 ./manage.py loaddata vendors/fixtures/naics.json
 ./manage.py loaddata vendors/fixtures/setasides.json
 ./manage.py loaddata vendors/fixtures/pools.json # reqs: naics
-./manage.py loaddata vendors/fixtures/vendors.json # reqs: setasides
+./manage.py loaddata vendors/fixtures/locations.json
+./manage.py loaddata vendors/fixtures/vendors.json # reqs: locations, setasides
 ./manage.py loaddata vendors/fixtures/poolpiids.json # reqs: pool, vendor
 ./manage.py loaddata vendors/fixtures/samloads.json
 
 # Load contract related fixtures
 
+./manage.py loaddata contract/fixtures/placesofperformance.json
 ./manage.py loaddata contract/fixtures/contracts.json #reqs: naics, vendor, piid
 ./manage.py loaddata contract/fixtures/fpdsloads.json #reqs: vendor
