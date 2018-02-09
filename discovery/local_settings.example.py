4de8be545b38b4e5bfb2ebca324eee57ea07da85
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-09 02:55:32 +0000

    Adding a dummy cache configuration in the local settings example file if DEBUG is enabled.

--- discovery/local_settings.example.py
+++ discovery/local_settings.example.py
@@ -6,3 +6,11 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 SAUCE = False
+
+if DEBUG:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+    
