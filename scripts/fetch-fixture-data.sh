6ef175f08dafa548cc6096059d26eb13095e7a02
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-10 07:51:14 +0000

    Adding start_date to the load_fpds calls in the fetch-fixture-data script.

--- scripts/fetch-fixture-data.sh
+++ scripts/fetch-fixture-data.sh
@@ -7,5 +7,5 @@ cd "$SCRIPT_DIR/.."
 
 ./manage.py load_categories
 ./manage.py load_vendors --vpp=0 --tries=3 --pause=1
-./manage.py load_fpds --reinit --period=520 --load=520 --count=50 --max=50 --pause=1
-./manage.py load_fpds --id=135 --period=520 --load=520 --count=500 --max=500 --pause=1
+./manage.py load_fpds --reinit --starting_date='2008-02-22' --load=520 --count=50 --max=50 --pause=1
+./manage.py load_fpds --id=135 --starting_date='2008-02-22' --load=520 --count=500 --max=500 --pause=1
