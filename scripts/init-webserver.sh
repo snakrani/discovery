06ed2f28afc052f775783f480799b5566ef1dc82
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-13 13:25:13 +0000

    Commenting out the collectstatic in the init-webserver script.

--- scripts/init-webserver.sh
+++ scripts/init-webserver.sh
@@ -4,7 +4,7 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
-cd "$SCRIPT_DIR/.."
+cd "$SCRIPT_DIR/../app"
 
 LOG_FILE="${1:-./logs/discovery-init.log}"
 if [ "$LOG_FILE" != "/dev/stdout" -a "$LOG_FILE" != "/dev/stderr" ]
@@ -21,5 +21,5 @@ fi
 
 #run application setup commands
 
-echo "> Collecting Django static files" | tee -a "$LOG_FILE"
-python manage.py collectstatic --noinput >>"$LOG_FILE" 2>&1
+#echo "> Collecting Django static files" | tee -a "$LOG_FILE"
+#python manage.py collectstatic --noinput >>"$LOG_FILE" 2>&1
