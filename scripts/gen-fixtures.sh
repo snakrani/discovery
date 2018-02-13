6ab24905b2d1eb28da263465e826bb7b71f28d11
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-13 13:29:37 +0000

    Updating the app directory location and contracts app name in the gen-fixtures and load_fixtures scripts.

--- scripts/gen-fixtures.sh
+++ scripts/gen-fixtures.sh
@@ -2,15 +2,7 @@
 # Generate data for the GSA PSHC discovery application, per the README instructions.
 
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
-cd "$SCRIPT_DIR/.."
-
-# Generate category fixtures
-
-./manage.py dumpdata vendors.naics --indent=2 > vendors/fixtures/naics.json
-./manage.py dumpdata vendors.setaside --indent=2 > vendors/fixtures/setasides.json
-./manage.py dumpdata vendors.zone --indent=2 > vendors/fixtures/zones.json
-./manage.py dumpdata vendors.zonestate --indent=2 > vendors/fixtures/zonestates.json # reqs: zones
-./manage.py dumpdata vendors.pool --indent=2 > vendors/fixtures/pools.json # reqs: naics
+cd "$SCRIPT_DIR/../app"
 
 # Generate vendor related fixtures
 
@@ -24,6 +16,6 @@ cd "$SCRIPT_DIR/.."
 
 # Generate contract related fixtures
 
-./manage.py dumpdata contract.placeofperformance --indent=2 > contract/fixtures/placesofperformance.json
-./manage.py dumpdata contract.contract --indent=2 > contract/fixtures/contracts.json #reqs: naics, vendor, piid
-./manage.py dumpdata contract.fpdsload --indent=2 > contract/fixtures/fpdsloads.json #reqs: vendor
+./manage.py dumpdata contracts.placeofperformance --indent=2 > contracts/fixtures/placesofperformance.json
+./manage.py dumpdata contracts.contract --indent=2 > contracts/fixtures/contracts.json #reqs: naics, vendor, piid
+./manage.py dumpdata contracts.fpdsload --indent=2 > contracts/fixtures/fpdsloads.json #reqs: vendor
