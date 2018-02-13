e74c07742f6fc406e6984fb2eaf69bbe75f9c630
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-13 13:26:17 +0000

    Updating the app directory location in the fetch-data script.

--- scripts/fetch-data.sh
+++ scripts/fetch-data.sh
@@ -5,7 +5,7 @@
 #
 
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
-cd "$SCRIPT_DIR/.."
+cd "$SCRIPT_DIR/../app"
 
 # TODO: Set vpp, period, max, and pause from options
 
