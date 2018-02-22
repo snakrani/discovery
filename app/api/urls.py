65e3772a4292996c16a2c8574bdb52737fe4f6fa
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-22 10:01:53 -0500

    Fixing the format of URL return in the API urls (related to Django upgrade).

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.decorators.cache import cache_page
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from api import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^naics/$', cache_page(60*60*48)(views.ListNaics.as_view())),
     url(r'^pools/$', cache_page(60*60*48)(views.ListPools.as_view())),
     url(r'^zones/$', cache_page(60*60*48)(views.ListZones.as_view())),
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
     url(r'^contracts/$', cache_page(60*60*24)(views.ListContracts.as_view())),
     
     url(r'^metadata/$', cache_page(60*60*24)(views.MetadataView.as_view())),
-)
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
