07d6070bfd187252a3ad945ad9fb8e91eaa51f3c
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-22 10:01:23 -0500

    Fixing the format of URL return in the discovery urls (related to Django upgrade).

--- app/discovery/urls.py
+++ app/discovery/urls.py
@@ -1,27 +1,25 @@
-from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles import views
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
-from django.conf import settings
+#from django.conf.urls.static import static
 
 from api import urls as api_urls
 from vendors.views import VendorView
 
-
 admin.autodiscover()
 
-
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^api/', include(api_urls)),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
+    #url(r'^docs/', get_swagger_view(title='Discovery API')),
     url(r'^results/$', TemplateView.as_view(template_name='pool.html')),
     url(r'^results/csv', 'vendors.views.pool_csv', name="pool-csv"),
     url(r'^vendor/(?P<vendor_duns>\w+)/$', VendorView.as_view(template_name='vendor.html')),
     url(r'^vendor/(?P<vendor_duns>\w+)/csv/$', 'vendors.views.vendor_csv', name="vendor-csv"),   
-    url(r'^admin/', include(admin.site.urls)),    
-)
+    url(r'^admin/', include(admin.site.urls))    
+]
 
 if settings.DEBUG:
     urlpatterns += [
