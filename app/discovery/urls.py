264044accf933dd007b87a70ec689e47b42df617
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-22 13:20:59 -0500

    Fixing upgrade issues with the discovery urls registry.

--- app/discovery/urls.py
+++ app/discovery/urls.py
@@ -3,22 +3,23 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles import views
 from django.views.generic import TemplateView
-#from django.conf.urls.static import static
 
 from api import urls as api_urls
-from vendors.views import VendorView
+from vendors.views import VendorView, PoolCSV, VendorCSV
+
 
 admin.autodiscover()
+
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^api/', include(api_urls)),
     #url(r'^docs/', get_swagger_view(title='Discovery API')),
     url(r'^results/$', TemplateView.as_view(template_name='pool.html')),
-    url(r'^results/csv', 'vendors.views.pool_csv', name="pool-csv"),
+    url(r'^results/csv', PoolCSV, name="pool-csv"),
     url(r'^vendor/(?P<vendor_duns>\w+)/$', VendorView.as_view(template_name='vendor.html')),
-    url(r'^vendor/(?P<vendor_duns>\w+)/csv/$', 'vendors.views.vendor_csv', name="vendor-csv"),   
-    url(r'^admin/', include(admin.site.urls))    
+    url(r'^vendor/(?P<vendor_duns>\w+)/csv/$', VendorCSV, name="vendor-csv"),   
+    url(r'^admin/', admin.site.urls)    
 ]
 
 if settings.DEBUG:
