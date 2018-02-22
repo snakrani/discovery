2ef86ab1709480428565845c5c9335d6aae49c1e
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-22 13:19:21 -0500

    Fixing upgrade issues with the categories migrations.

--- app/categories/migrations/0001_initial.py
+++ app/categories/migrations/0001_initial.py
@@ -52,7 +52,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('state', models.CharField(max_length=50)),
-                ('zone', models.ForeignKey(related_name='state', to='categories.Zone', null=True)),
+                ('zone', models.ForeignKey(related_name='state', to='categories.Zone', null=True, on_delete=models.CASCADE)),
             ],
         ),
     ]
