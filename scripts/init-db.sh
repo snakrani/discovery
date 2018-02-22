29d4dad71e8d6074ad65d6b55a976ac58061a0dc
Adrian Webb <adrianwebb.78@gmail.com>
2018-02-22 09:56:57 -0500

    Updating the init-db script to use Python 3.

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -15,7 +15,8 @@ fi
 #activate the virtual python environment
 if [ -d /venv ]
 then
-  alias python="/venv/bin/python"
+  echo "> Activating Python virtual environment" | tee -a "$LOG_FILE"
+  alias python3="/venv/bin/python3"
   source /venv/bin/activate
 fi
 
@@ -34,16 +35,16 @@ fi
 #run application setup commands
 
 echo "> Migrating Django database structure" | tee -a "$LOG_FILE"
-python manage.py migrate --noinput >>"$LOG_FILE" 2>&1
+python3 manage.py migrate --noinput >>"$LOG_FILE" 2>&1
 
 echo "> Ensuring Django cache table" | tee -a "$LOG_FILE"
-python manage.py createcachetable >>"$LOG_FILE" 2>&1
+python3 manage.py createcachetable >>"$LOG_FILE" 2>&1
 
 echo "> Loading basic category information" | tee -a "$LOG_FILE"
-python manage.py load_categories >>"$LOG_FILE" 2>&1
+python3 manage.py load_categories >>"$LOG_FILE" 2>&1
 
 echo "> Clearing outdated locks" | tee -a "$LOG_FILE"
-python manage.py clear_locks >>"$LOG_FILE" 2>&1
+python3 manage.py clear_locks >>"$LOG_FILE" 2>&1
 
 # Create admin user ONLY IF it doesn't exist yet
 "$SCRIPT_DIR/create-admin.sh" admin admin-changeme >>"$LOG_FILE" 2>&1
