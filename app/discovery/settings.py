2a4ed7b86bb7cadf59a3d5a8fe2cc8f651d5629f
Adrian Webb <adrian@webb.sh>
2018-02-25 17:02:41 +0000

    Fixing context processors in the settings.

--- app/discovery/settings.py
+++ app/discovery/settings.py
@@ -152,13 +152,15 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.contrib.auth.context_processors.auth',
+                'django.core.context_processors.auth',
                 'django.core.context_processors.debug',
                 'django.core.context_processors.i18n',
                 'django.core.context_processors.media',
-                'django.core.context_processors.request',
                 'django.core.context_processors.static',
                 'django.core.context_processors.tz',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'discovery.context_processors.api_host',
             ],
