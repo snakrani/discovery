1c76dd2fadd7a8ba408cb86c4e9c8d4a0f9a87ce
Adrian Webb <adrian@webb.sh>
2018-03-01 10:54:06 -0500

    Some updates to the main Discovery site url configuration.

--- app/discovery/urls.py
+++ app/discovery/urls.py
@@ -1,28 +1,33 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib import admin
-from django.contrib.staticfiles import views
 from django.views.generic import TemplateView
+from django.contrib import admin
+from django.contrib.staticfiles import views as static_views
+
+from rest_framework.documentation import include_docs_urls
 
 from api import urls as api_urls
-from vendors.views import VendorView, PoolCSV, VendorCSV
+from vendors import views as vendors
 
 
 admin.autodiscover()
 
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
+    
     url(r'^api/', include(api_urls)),
-    #url(r'^docs/', get_swagger_view(title='Discovery API')),
+    url(r'^docs/', include_docs_urls(title="Discovery API", public=True)),
+    
     url(r'^results/$', TemplateView.as_view(template_name='pool.html')),
-    url(r'^results/csv', PoolCSV, name="pool-csv"),
-    url(r'^vendor/(?P<vendor_duns>\w+)/$', VendorView.as_view(template_name='vendor.html')),
-    url(r'^vendor/(?P<vendor_duns>\w+)/csv/$', VendorCSV, name="vendor-csv"),   
-    url(r'^admin/', admin.site.urls)    
+    url(r'^results/csv', vendors.PoolCSV, name="pool-csv"),
+    
+    url(r'^vendor/(?P<vendor_duns>\w+)/$', vendors.VendorView.as_view(template_name='vendor.html')),
+    url(r'^vendor/(?P<vendor_duns>\w+)/csv/$', vendors.VendorCSV, name="vendor-csv"),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        url(r'^static/(?P<path>.*)$', views.serve),
+        url(r'^static/(?P<path>.*)$', static_views.serve),
     ]
