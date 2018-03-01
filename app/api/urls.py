f92c7e8a1c79a6b5ea3f0e029b03925102dbb227
Adrian Webb <adrian@webb.sh>
2018-03-01 10:58:24 -0500

    Refactoring the API url configurations to use the new ViewSet and Router architecture.

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,20 +1,14 @@
-from django.conf.urls import include, url
-from django.views.decorators.cache import cache_page
-from rest_framework.urlpatterns import format_suffix_patterns
+from api import routers, views
 
-from api import views
 
-urlpatterns = [
-    url(r'^naics/$', cache_page(60*60*48)(views.ListNaics.as_view())),
-    url(r'^pools/$', cache_page(60*60*48)(views.ListPools.as_view())),
-    url(r'^zones/$', cache_page(60*60*48)(views.ListZones.as_view())),
-    
-    url(r'^vendors/$', cache_page(60*60*24)(views.ListVendors.as_view())),
-    url(r'^vendor/(?P<duns>\w+)', cache_page(60*60*24)(views.GetVendor.as_view())),
-    
-    url(r'^contracts/$', cache_page(60*60*24)(views.ListContracts.as_view())),
-    
-    url(r'^metadata/$', cache_page(60*60*24)(views.MetadataView.as_view())),
-]
+router = routers.OptionalSlashRouter()
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+router.register(r'naics', views.NaicsViewSet)
+router.register(r'setasides', views.SetAsideViewSet)
+router.register(r'pools', views.PoolViewSet)
+router.register(r'zones', views.ZoneViewSet)
+router.register(r'vendors', views.VendorViewSet)
+router.register(r'contracts', views.ContractViewSet)
+#router.register(r'metadata', views.MetadataViewSet)
+
+urlpatterns = router.urls
