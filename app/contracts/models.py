838f52ae538c6a1a9c42df7b2547df95e9c85874
Adrian Webb <adrian@webb.sh>
2018-03-05 14:32:40 -0500

    Adding pricing structure and contract status models to the contract models.

--- app/contracts/models.py
+++ app/contracts/models.py
@@ -4,54 +4,19 @@ from categories.models import Naics
 from vendors.models import Location, Vendor
 
 
-PRICING_CHOICES = (
-    ('A', 'Fixed Price Redetermination'),
-    ('B', 'Fixed Price Level of Effort'),
-    ('J', 'Firm Fixed Price'),
-    ('K', 'Fixed Price with Economic Price Adjustment'),
-    ('L', 'Fixed Price Incentive'),
-    ('M', 'Fixed Price Award Fee'),
-    ('R', 'Cost Plus Award Fee'),
-    ('S', 'Cost No Fee'),
-    ('T', 'Cost Sharing'),
-    ('U', 'Cost Plus Fixed Fee'),
-    ('V', 'Cost Plus Incentive Fee'),
-    ('Y', 'Time and Materials'),
-    ('Z', 'Labor Hours'),
-    ('1', 'Order Dependent'),
-    ('2', 'Combination'),
-    ('3', 'Other'),
-)
-
-REASON_FOR_MODIFICATION_CHOICES = (
-    ('A', 'Additional Work'),
-    ('B', 'Supplemental Agreement for work within scope'),
-    ('C', 'Funding Only Action'),
-    ('D', 'Change Order'),
-    ('E', 'Terminated for Default'),
-    ('F', 'Terminated for Convenience'),
-    ('G', 'Exercise an Option'),
-    ('H', 'Definitize Letter Contract'),
-    ('J', 'Novation Agreement'),
-    ('K', 'Close out'),
-    ('L', 'Definitize Letter Contract'),
-    ('M', 'Other Adminitrative Action'),
-    ('N', 'Legal Contract Cancellation'),
-    ('P', 'Representation of non-Novated Merger/Acquisitoin'),
-    ('R', 'Rerepresentation'),
-    ('S', 'Change PIID'),
-    ('T', 'Transfer Action'),
-    ('V', 'Vendor DUNS Change'),
-    ('W', 'Vendor Address Change'),
-    ('X', 'Terminated for Cause'),
-    ('C1', 'Completed'),
-    ('C2', 'Current'),
-)
-
-
 class FPDSLoad(models.Model):
     vendor = models.OneToOneField(Vendor, null=True, on_delete=models.CASCADE)
     load_date = models.DateField()
+
+
+class ContractStatus(models.Model):
+    code = models.CharField(max_length=5, null=False, primary_key=True)
+    name = models.CharField(max_length=128, null=False)
+
+
+class PricingStructure(models.Model):
+    code = models.CharField(max_length=5, null=False, primary_key=True)
+    name = models.CharField(max_length=128, null=False)
 
 
 class PlaceOfPerformance(models.Model):
@@ -79,10 +44,10 @@ class Contract(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     point_of_contact = models.EmailField(null=True)
     
-    pricing_type = models.CharField(choices=PRICING_CHOICES, max_length=2, null=True)
+    pricing_type = models.ForeignKey(PricingStructure, null=True, on_delete=models.DO_NOTHING)
     obligated_amount = models.DecimalField(max_digits=128, decimal_places=2, null=True)
     
-    reason_for_modification = models.CharField(choices=REASON_FOR_MODIFICATION_CHOICES, max_length=2, null=True)
+    status = models.ForeignKey(ContractStatus, null=True, on_delete=models.DO_NOTHING)
     
     annual_revenue = models.BigIntegerField(null=True)
     number_of_employees = models.IntegerField(null=True)
