4a160331a0f12102a1b66afe9d120e17611efbc2
Adrian Webb <adrian@webb.sh>
2018-03-12 11:02:10 +0000

    Moving filter assignments into top level constants and fixing zone state filters.

--- app/api/filters.py
+++ app/api/filters.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from rest_framework.fields import CharField, IntegerField
 
 from rest_framework_filters.filterset import FilterSet
-from rest_framework_filters.filters import NumberFilter, CharFilter, RelatedFilter
+from rest_framework_filters.filters import NumberFilter, CharFilter, RelatedFilter, BaseInFilter
 from rest_framework_filters.backends import ComplexFilterBackend
 
 from categories import models as categories
@@ -46,7 +46,7 @@ DATE_FILTERS = [
 ]
 
 NUM_FILTERS = [
-    'exact', 
+    'exact',
     'lt',
     'lte', 
     'gt', 
@@ -54,6 +54,96 @@ NUM_FILTERS = [
     'range',
     'in'
 ]
+
+
+NAICS_FIELDS = {
+    'code': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'root_code': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'description': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
+}
+
+SETASIDE_FIELDS = {
+    'code': EQUALITY_CHAR_FILTERS, 
+    'name': EQUALITY_CHAR_FILTERS, 
+    'description': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
+    'far_order': NUM_FILTERS
+}
+
+POOL_FIELDS = {
+    'id': EQUALITY_CHAR_FILTERS, 
+    'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'number': EQUALITY_CHAR_FILTERS, 
+    'vehicle': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'threshold': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
+}
+
+ZONE_FIELDS = {
+    'id': NUM_FILTERS
+}
+
+LOCATION_FIELDS = {
+    'address': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'city': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
+    'state': EQUALITY_CHAR_FILTERS,
+    'zipcode': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
+    'congressional_district': EQUALITY_CHAR_FILTERS
+}
+
+MANAGER_FIELDS = {
+    'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'type': EQUALITY_CHAR_FILTERS,
+    'phone': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
+    'email': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
+}
+
+VENDOR_FIELDS = {
+    'id': NUM_FILTERS, 
+    'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'duns': EQUALITY_CHAR_FILTERS, 
+    'cage': EQUALITY_CHAR_FILTERS, 
+    'sam_status': EQUALITY_CHAR_FILTERS, 
+    'sam_activation_date': DATE_FILTERS, 
+    'sam_expiration_date': DATE_FILTERS, 
+    'sam_exclusion': EQUALITY_BOOL_FILTERS, 
+    'sam_url': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
+}
+
+PLACE_OF_PERFORMANCE_FIELDS = {
+    'country_code': EQUALITY_CHAR_FILTERS, 
+    'country_name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
+    'state': EQUALITY_CHAR_FILTERS,
+    'zipcode': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
+}
+
+PRICING_STRUCTURE_FIELDS = {
+    'code': EQUALITY_CHAR_FILTERS, 
+    'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
+}
+
+CONTRACT_STATUS_FIELDS = {
+    'code': EQUALITY_CHAR_FILTERS, 
+    'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
+}
+
+CONTRACT_FIELDS = {
+    'id': NUM_FILTERS, 
+    'piid': NUM_FILTERS, 
+    'agency_id': EQUALITY_CHAR_FILTERS, 
+    'agency_name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'NAICS': EQUALITY_CHAR_FILTERS, 
+    'PSC': EQUALITY_CHAR_FILTERS,
+    'date_signed': DATE_FILTERS, 
+    'completion_date': DATE_FILTERS,
+    'obligated_amount': NUM_FILTERS,
+    'point_of_contact': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'vendor_phone': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
+    'annual_revenue': NUM_FILTERS, 
+    'number_of_employees': NUM_FILTERS
+}
+
+
+class CharInFilter(BaseInFilter, CharFilter):
+    pass
 
 
 class DiscoveryComplexFilterBackend(ComplexFilterBackend):
@@ -67,22 +157,13 @@ class DiscoveryComplexFilterBackend(ComplexFilterBackend):
 class NaicsFilter(FilterSet):
     class Meta:
         model = categories.Naics
-        fields = {
-            'code': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'root_code': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'description': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
-        }
+        fields = NAICS_FIELDS
 
 
 class SetAsideFilter(FilterSet):
     class Meta:
         model = categories.SetAside
-        fields = {
-            'code': EQUALITY_CHAR_FILTERS, 
-            'name': EQUALITY_CHAR_FILTERS, 
-            'description': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
-            'far_order': NUM_FILTERS
-        }
+        fields = SETASIDE_FIELDS
 
 
 class PoolFilter(FilterSet):
@@ -90,36 +171,23 @@ class PoolFilter(FilterSet):
     
     class Meta:
         model = categories.Pool
-        fields = {
-            'id': EQUALITY_CHAR_FILTERS, 
-            'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'number': NUM_FILTERS, 
-            'vehicle': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'threshold': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
-        }
+        fields = POOL_FIELDS
 
 
 class ZoneFilter(FilterSet):
-    state = CharFilter(field_name="state__state")
+    state = CharFilter(field_name="state__state", lookup_expr='exact')
+    state__iexact = CharFilter(field_name="state__state", lookup_expr='iexact')
+    state__in = CharInFilter(field_name="state__state", lookup_expr='in')
      
     class Meta:
         model = categories.Zone
-        fields = {
-            'id': NUM_FILTERS,
-            'state': EQUALITY_CHAR_FILTERS
-        }
+        fields = ZONE_FIELDS
 
 
 class LocationFilter(FilterSet):
     class Meta:
         model = vendors.Location
-        fields = {
-            'address': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'city': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
-            'state': EQUALITY_CHAR_FILTERS,
-            'zipcode': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
-            'congressional_district': EQUALITY_CHAR_FILTERS
-        }
+        fields = LOCATION_FIELDS
 
 
 class ManagerFilter(FilterSet):
@@ -128,12 +196,7 @@ class ManagerFilter(FilterSet):
     
     class Meta:
         model = vendors.Manager
-        fields = {
-            'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'type': EQUALITY_CHAR_FILTERS,
-            'phone': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
-            'email': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
-        }
+        fields = MANAGER_FIELDS
 
 
 class VendorFilter(FilterSet):
@@ -146,46 +209,25 @@ class VendorFilter(FilterSet):
     
     class Meta:
         model = vendors.Vendor
-        fields = {
-            'id': NUM_FILTERS, 
-            'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'duns': EQUALITY_CHAR_FILTERS, 
-            'cage': EQUALITY_CHAR_FILTERS, 
-            'sam_status': EQUALITY_CHAR_FILTERS, 
-            'sam_activation_date': DATE_FILTERS, 
-            'sam_expiration_date': DATE_FILTERS, 
-            'sam_exclusion': EQUALITY_BOOL_FILTERS, 
-            'sam_url': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
-        }
+        fields = VENDOR_FIELDS
 
 
 class ContractStatusFilter(FilterSet):
     class Meta:
         model = contracts.ContractStatus
-        fields = {
-            'code': EQUALITY_CHAR_FILTERS, 
-            'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
-        }
+        fields = CONTRACT_STATUS_FIELDS
 
 
 class PricingStructureFilter(FilterSet):
     class Meta:
         model = contracts.PricingStructure
-        fields = {
-            'code': EQUALITY_CHAR_FILTERS, 
-            'name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
-        }
+        fields = PRICING_STRUCTURE_FIELDS
 
 
 class PlaceOfPerformanceFilter(FilterSet):
     class Meta:
         model = contracts.PlaceOfPerformance
-        fields = {
-            'country_code': EQUALITY_CHAR_FILTERS, 
-            'country_name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS,
-            'state': EQUALITY_CHAR_FILTERS,
-            'zipcode': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS
-        }
+        fields = PLACE_OF_PERFORMANCE_FIELDS
 
 
 class ContractFilter(FilterSet): 
@@ -199,18 +241,4 @@ class ContractFilter(FilterSet):
     
     class Meta:
         model = contracts.Contract
-        fields = {
-            'id': NUM_FILTERS, 
-            'piid': NUM_FILTERS, 
-            'agency_id': EQUALITY_CHAR_FILTERS, 
-            'agency_name': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'NAICS': EQUALITY_CHAR_FILTERS, 
-            'PSC': EQUALITY_CHAR_FILTERS,
-            'date_signed': DATE_FILTERS, 
-            'completion_date': DATE_FILTERS,
-            'obligated_amount': NUM_FILTERS,
-            'point_of_contact': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'vendor_phone': FUZZY_CHAR_FILTERS + EQUALITY_CHAR_FILTERS, 
-            'annual_revenue': NUM_FILTERS, 
-            'number_of_employees': NUM_FILTERS
-        }
+        fields = CONTRACT_FIELDS
