7a8f59084d12292eceb6f44df685d610b9e0edd8
Adrian Webb <adrian@webb.sh>
2018-03-12 11:58:44 +0000

    Updated test_naics api tests.

--- app/api/tests/test_naics.py
+++ app/api/tests/test_naics.py
@@ -12,48 +12,35 @@ class BaseNaicsTest(test.CategoryAPITestCase):
 
 
 class NaicsListTest(BaseNaicsTest):
-    
-    def test_no_args(self):
-        self.validated_list(55)
-        
-    
-    def test_search_found_1(self):
-        resp = self.validated_list(1, q = 'Environmental')
-        resp.validate(lambda resp, base_key: resp.matches(base_key + ['description'], 'Environmental'))
-    
-    def test_search_found_2(self):
-        resp = self.validated_list(1, q = '541910')
-        resp.validate(lambda resp, base_key: resp.equal(base_key + ['root_code'], '541910'))
-    
-    def test_search_not_found(self):
-        self.empty_list(q = '0000000000000')
-    
-    
-    def test_ordering_code(self):
-        resp = self.validated_list(55, ordering = 'code')
-        resp.validate_ordering('code', 'asc')
-    
-    def test_ordering_root_code(self):
-        resp = self.validated_list(55, ordering = '-root_code')
-        resp.validate_ordering('root_code', 'desc')
-    
-    def test_ordering_description(self):
-        resp = self.validated_list(55, ordering = 'description')
-        resp.validate_ordering('description', 'asc')
+    def schema(self):
+        return {
+            'ordering': ('code', 'root_code', 'description'),
+            'pagination': {
+                '@no_args': {},
+                '!page': {'page': 3},
+                '@count': {'count': 5},
+                '@mixed': {'page': 4, 'count': 10}
+            },
+            'search': {
+                '*search1': ('description', 'matches', 'Environmental'),
+                '*search2': ('root_code', 'equal', '541910'),
+                '-search3': ('code', 'matches', '0000000000000')
+            }
+        }
     
     
     def test_mixed_request_found_1(self):
-        resp = self.validated_list(9, q = 'Maintenance', ordering = '-code')
+        resp = self.validated_multi_list(q = 'Maintenance', ordering = '-code')
         resp.validate(lambda resp, base_key: resp.matches(base_key + ['description'], 'Maintenance'))
         resp.validate_ordering('code', 'desc')
     
     def test_mixed_request_found_2(self):
-        resp = self.validated_list(1, q = 'Pest', ordering = 'code')
+        resp = self.validated_multi_list(q = 'Pest', ordering = 'code')
         resp.validate(lambda resp, base_key: resp.matches(base_key + ['description'], 'Pest'))
         resp.validate_ordering('code', 'asc')
     
     def test_mixed_request_found_3(self):
-        resp = self.validated_list(26, q = 'Services', ordering = '-description')
+        resp = self.validated_multi_list(q = 'Services', ordering = '-description')
         resp.validate(lambda resp, base_key: resp.matches(base_key + ['description'], 'Services'))
         resp.validate_ordering('description', 'desc')
     
@@ -64,38 +51,14 @@ class NaicsListTest(BaseNaicsTest):
         self.empty_list(q = 'Arghhhhh!!', ordering = '-description')
     
 
-    def test_pagination_no_args(self):
-        resp = self.validated_list(55)
-        resp.validate_pagination(1)    
-    
-    def test_pagination_page_requested(self):
-        self.invalid_list(page = 3)
-    
-    def test_pagination_count_requested(self):
-        resp = self.validated_list(55, count = 5)
-        resp.validate_pagination(1, 5)
-    
-    def test_pagination_page_count_requested(self):
-        resp = self.validated_list(55, page = 4, count = 10)
-        resp.validate_pagination(4, 10)
-
-
 class NaicsRetrieveTest(BaseNaicsTest):
-    
-    def test_found_1(self):
-        resp = self.validated_object('541614')
-        resp.equal('root_code', '541614')
-    
-    def test_found_2(self):
-        resp = self.validated_object('541330D')
-        resp.equal('root_code', '541330')
-    
-    def test_found_3(self):
-        resp = self.validated_object('541840')
-        resp.equal('root_code', '541840')
-    
-    def test_not_found_1(self):
-        self.invalid_object('77777777')
-    
-    def test_not_found_2(self):
-        self.invalid_object('ABCDEFG')
+    def schema(self):
+        return {
+            'object': {
+                '&541614': ('root_code', 'iequal', '541614'),
+                '&541330D': ('root_code', 'iequal', '541330'),
+                '&541840': ('root_code', 'iequal', '541840'),
+                '#77777777': (),
+                '#ABCDEFG': ()
+            }
+        }
