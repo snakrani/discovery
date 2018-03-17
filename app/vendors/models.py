b014f89a73d891830fd882ea86a6e7fa284d9150
Adrian Webb <adrian@webb.sh>
2018-03-17 00:09:40 +0000

    Updating the vendor models to fit the vendor pool data more effecively.

--- app/vendors/models.py
+++ app/vendors/models.py
@@ -3,12 +3,6 @@ from django.db import models
 from categories.models import SetAside, Pool, Zone
 
 
-MANAGEMENT_TYPES = (
-    ('CM', 'Contract Manager'),
-    ('PM', 'Project Manager')
-)
-
-
 class SamLoad(models.Model):
     sam_load = models.DateField()
 
@@ -39,27 +33,49 @@ class Vendor(models.Model):
     
     sam_url = models.URLField(null=True) # from SAM
     sam_location = models.ForeignKey(Location, null=True, on_delete=models.CASCADE) # from SAM
-  
-    pools = models.ManyToManyField(Pool, through='PoolMembership') # from CSV
-    setasides = models.ManyToManyField(SetAside, blank=True) # from CSV
-
+    
     def __str__(self):
         return self.name
 
 
+class PoolMembership(models.Model):
+    vendor = models.ForeignKey(Vendor, related_name='pools', on_delete=models.CASCADE)
+    pool = models.ForeignKey(Pool, on_delete=models.DO_NOTHING)
+    piid = models.CharField(max_length=128) # from CSV
+    
+    setasides = models.ManyToManyField(SetAside, blank=True) # from CSV
+    
+    def __str__(self):
+        return "{0} - {1}/{2} ({3})".format(self.vendor.name, self.pool.id, self.zone, self.piid)
+
+
+class PoolMembershipZone(models.Model):
+    membership = models.ForeignKey(PoolMembership, null=True, related_name='zone', on_delete=models.CASCADE)
+    zone = models.ForeignKey(Zone, null=True, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return "{0}".format(self.zone)
+
+
 class Manager(models.Model):
-    vendor = models.ForeignKey(Vendor, null=True, related_name='managers', on_delete=models.CASCADE)
     name = models.CharField(null=True, max_length=128)
-    type = models.CharField(choices=MANAGEMENT_TYPES, max_length=10)
+    
+    def __str__(self):
+        return "{0}".format(self.name)
     
     def phones(self):
         return self.phone.values_list('number', flat=True)
     
     def emails(self):
         return self.email.values_list('address', flat=True)
+    
 
-    def __str__(self):
-        return "{0}: {1} ({2})".format(self.type, self.name, self.vendor.name)
+class ContractManager(Manager):
+    responsibility = models.ForeignKey(PoolMembership, null=True, related_name='cms', on_delete=models.DO_NOTHING)
+
+
+class ProjectManager(Manager):
+    responsibility = models.ForeignKey(PoolMembership, null=True, related_name='pms', on_delete=models.DO_NOTHING)
 
 
 class ManagerPhoneNumber(models.Model):
@@ -76,23 +92,3 @@ class ManagerEmail(models.Model):
 
     def __str__(self):
         return "{0} ({1})".format(self.manager.name, self.address)
-    
-
-class PoolMembership(models.Model):
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    pool = models.ForeignKey(Pool, on_delete=models.DO_NOTHING)
-    piid = models.CharField(max_length=128)
-    
-    def zones(self):
-        return self.zone.values_list('zone', flat=True)
-    
-    def __str__(self):
-        return "{0} - {1}/{2} ({3})".format(self.vendor.name, self.pool.id, self.zone, self.piid)
-
-
-class PoolMembershipZone(models.Model):
-    membership = models.ForeignKey(PoolMembership, null=True, related_name='zone', on_delete=models.CASCADE)
-    zone = models.ForeignKey(Zone, null=True, on_delete=models.DO_NOTHING)
-
-    def __str__(self):
-        return "{0} ({1} | {2})".format(self.membership.vendor.name, self.pool.name, self.zone)
