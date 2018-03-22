bda3acf073f514a890693d2897039a6f0994bb53
Adrian Webb <adrian@webb.sh>
2018-03-22 06:27:58 +0000

    Stripping down the contracts test library.

--- app/contracts/tests.py
+++ app/contracts/tests.py
@@ -1,17 +1,13 @@
 from test import cases as case
 from test import fixtures as data
 
-import os
-from django.test import TestCase
 from django.core.management import call_command
 
 
-class FPDSLoaderTest(TestCase):
+class FPDSLoaderTest(case.BaseTestCase):
 
     fixtures = data.get_vendor_fixtures()
 
 
     def test_load(self):
-        call_command('load_fpds', id=89, period=52, load=52, pause=0, max=1)        
-
-    #ToDO: methods to check values of certain contracts loaded from above command
+        call_command('load_fpds', id=89, period=52, load=52, pause=0, max=1)
