c81f2a7a68cffd48837b12cf895e3804c09befdb
Adrian Webb <adrian@webb.sh>
2018-03-22 06:14:54 +0000

    Updating the fixture data location in the contract tests definition.

--- app/contracts/tests.py
+++ app/contracts/tests.py
@@ -1,12 +1,15 @@
+from test import cases as case
+from test import fixtures as data
+
 import os
 from django.test import TestCase
 from django.core.management import call_command
 
-from discovery.fixtures import get_vendor_fixtures
 
 class FPDSLoaderTest(TestCase):
-    """Tests the load_fpds management command"""
-    fixtures = get_vendor_fixtures()
+
+    fixtures = data.get_vendor_fixtures()
+
 
     def test_load(self):
         call_command('load_fpds', id=89, period=52, load=52, pause=0, max=1)        
