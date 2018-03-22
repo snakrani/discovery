0b98fa74f1aa66bbc758e935931a97cb741859f1
Adrian Webb <adrian@webb.sh>
2018-03-22 06:11:36 +0000

    Updating and adding more smoke tests in the discovery tests definition.

--- app/discovery/tests.py
+++ app/discovery/tests.py
@@ -1,15 +1,29 @@
-from django.test import Client, TestCase
+from test import cases as case
+from test import fixtures as data
 
-from discovery.fixtures import get_vendor_fixtures
+
+class SmokeTest(case.RequestTestCase):
+    
+    fixtures = data.get_vendor_fixtures()
 
 
-class SmokeTest(TestCase):
-    """smoke tests for discovery site"""
-    fixtures = get_vendor_fixtures()
+    def test_home_found(self):
+        self.validated_path('/')
 
-    def setUp(self):
-        self.c = Client()
-
-    def test_index_page_loads(self):
-        resp = self.c.get('/')
-        self.assertEqual(resp.status_code, 200)
+    def test_docs_found(self):
+        self.validated_path('/docs/')
+        
+    def test_admin_found(self):
+        self.validated_temp_redirect('/admin/')
+        
+    def test_results_found(self):
+        self.validated_path('/results/')
+    
+    def test_results_csv_found(self):
+        self.validated_path('/results/csv/', **{'vehicle': 'HCATS_SB', 'naics-code': 611710})
+    
+    def test_vendor_found(self):
+        self.validated_path('/vendor/079939977/')
+    
+    def test_vendor_csv_found(self):
+        self.validated_path('/vendor/079939977/csv/')
