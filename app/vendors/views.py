4d32af1bd6ea870c242a5e6a74225764db0ff216
Adrian Webb <adrian@webb.sh>
2018-03-24 12:54:50 +0000

    Fixing contextual loading issues with the VendorCSV view in the vendors views.

--- app/vendors/views.py
+++ app/vendors/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.generic import TemplateView
 
 from categories.models import Naics, SetAside, Pool
-from vendors.models import Vendor
+from vendors.models import Vendor, ProjectManager
 from contracts.models import Contract
 
 import csv
@@ -99,8 +99,15 @@ def PoolCSV(request):
 def VendorCSV(request, vendor_duns):
     vendor = Vendor.objects.get(duns=vendor_duns)
     setasides = SetAside.objects.all().order_by('far_order')
+    
+    vendor_sa = []
+    pm_names = []
+    pm_phones = []
+    pm_emails = []
 
+    vehicle = request.GET.get('vehicle', 'None')
     naics = request.GET.get('naics-code', None)
+    
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="search_results.csv"'
     writer = csv.writer(response)
@@ -108,26 +115,52 @@ def VendorCSV(request, vendor_duns):
     writer.writerow((vendor.name,))
     writer.writerow(('SAM registration expires: ', vendor.sam_expiration_date.strftime("%m/%d/%Y")))
     writer.writerow(('', ))
-    writer.writerow([sa_obj.name for sa_obj in setasides])
-
-    vendor_sa = []
-    for sa in setasides:
-        if sa.id in vendor.pools.values_list('setasides', flat=True):
-            vendor_sa.append('X')
-        else:
-            vendor_sa.append('')
+    writer.writerow([''] + [sa_obj.name for sa_obj in setasides])
+    
+    if vehicle and naics:
+        naics = Naics.objects.get(code=naics)
+        pool = Pool.objects.get(naics=naics, vehicle=vehicle.upper())
+        membership = None
+        
+        for mem in vendor.pools.all():
+            if pool.id == mem.pool.id:
+                membership = mem
+                break
+        
+        if membership:
+            for sa in setasides:
+                if sa.id in membership.setasides.values_list('id', flat=True):
+                    vendor_sa.append('X')
+                else:
+                    vendor_sa.append('')
             
-    pm = vendor.pools.first().pms.first()
-    
-    if pm:
-        pm_name = pm.name
-        pm_phones = ",".join(pm.phone())
-        pm_emails = ",".join(pm.email())
+            if membership.pms.count():
+                for pm in membership.pms.all():
+                    pm_names.append(pm.name)
+                    pm_phones.append(",".join(pm.phone()))
+                    pm_emails.append(",".join(pm.email()))
+            else:
+                pm_names = ['NA']
+                pm_phones = ['NA']
+                pm_emails = ['NA']
     else:
-        pm_name = 'NA'
-        pm_phones = 'NA'
-        pm_emails = 'NA'
+        for sa in setasides:
+            if sa.id in vendor.pools.values_list('setasides', flat=True):
+                vendor_sa.append('X')
+            else:
+                vendor_sa.append('')
     
+        if vendor.pools.count():
+            for id in vendor.pools.all().values_list('pms', flat=True):
+                pm = ProjectManager.objects.get(manager_ptr_id=id)
+            
+                pm_names.append(pm.name)
+                pm_phones.append(",".join(pm.phone()))
+                pm_emails.append(",".join(pm.email()))
+        else:
+            pm_names = ['NA']
+            pm_phones = ['NA']
+            pm_emails = ['NA']
     
     contracts = Contract.objects.filter(vendor=vendor).order_by('-date_signed')[:1]
     
@@ -140,32 +173,29 @@ def VendorCSV(request, vendor_duns):
         annual_revenue = 'NA'
         
 
-    writer.writerow(vendor_sa)
+    writer.writerow([''] + vendor_sa)
     writer.writerow(('', ))
     writer.writerow(('DUNS', vendor.duns, '', 'Address:', titlecase(vendor.sam_location.address)))
     writer.writerow(('CAGE Code', vendor.cage, '', '',  titlecase(vendor.sam_location.state) + ', ' + vendor.sam_location.zipcode))
-    writer.writerow(('Employees', number_of_employees, '', 'POC:', pm_name))
-    writer.writerow(('Annual Revenue', annual_revenue, '', '', pm_phones))
-    writer.writerow(('', '', '', '', pm_emails))
+    writer.writerow(['Employees', number_of_employees, '', 'POC:'] + pm_names)
+    writer.writerow(['Annual Revenue', annual_revenue, '', ''] + pm_phones)
+    writer.writerow(['', '', '', ''] + pm_emails)
     writer.writerow(('', ))
+    
     if naics:
-        writer.writerow(('This vendor\'s contract history for NAICS {0}'.format(naics), ))
+        writer.writerow(('This vendor\'s contract history for NAICS {0}'.format(naics.code), ))
+        writer.writerow(('{0}'.format(naics.description), ))
     else:
         writer.writerow(('This vendor\'s contract history for all contracts', ))
 
     writer.writerow(('Date Signed', 'PIID', 'Agency', 'Type', 'Value ($)', 'Email POC', 'Status'))
 
     if naics:
-        contracts = Contract.objects.filter(vendor=vendor, NAICS=naics).order_by('-date_signed')
+        contracts = Contract.objects.filter(vendor=vendor, NAICS=naics.root_code).order_by('-date_signed')
     else:
         contracts = Contract.objects.filter(vendor=vendor).order_by('-date_signed')
 
     for c in contracts:
-        if '_' in c.piid:
-            piid = c.piid.split('_')[1]
-        else:
-            piid = c.piid
-        
-        writer.writerow((c.date_signed.strftime("%m/%d/%Y"), piid, titlecase(c.agency_name), c.pricing_type.name, c.obligated_amount, (c.point_of_contact or "").lower(), c.status.name))
+        writer.writerow((c.date_signed.strftime("%m/%d/%Y"), c.piid, titlecase(c.agency_name), c.pricing_type.name, c.obligated_amount, (c.point_of_contact or "").lower(), c.status.name))
 
     return response
