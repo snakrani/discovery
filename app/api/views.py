984dfebe782b00002d35d4431babceb5ac920b2d
Adrian Webb <adrian@webb.sh>
2018-03-25 18:01:21 +0000

    Caching the metadata view hourly.

--- app/api/views.py
+++ app/api/views.py
@@ -267,7 +267,7 @@ class ContractViewSet(DiscoveryReadOnlyModelViewSet):
         )
 
 
-@method_decorator(cache_page(60*60*settings.API_CACHE_LIFETIME), name='get')
+@method_decorator(cache_page(60*60), name='get')
 class ListMetadataView(APIView):
     """
     This endpoint returns metadata for the most recent data loads of SAM and FPDS data. It takes no parameters.
