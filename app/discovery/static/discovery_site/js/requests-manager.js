e03ebacb849093a7551f8481254e3cda89a310d1
Adrian Webb <adrian@webb.sh>
2018-03-28 12:08:18 +0000

    Updating the requests-manager getAPIRequest function to allow for a passed-in error_callback.

--- app/discovery/static/discovery_site/js/requests-manager.js
+++ app/discovery/static/discovery_site/js/requests-manager.js
@@ -20,21 +20,22 @@ var RequestsManager = {
     load: function() {
     },
 
-    getAPIRequest: function(url, params, callback) {
+    getAPIRequest: function(url, params, success_callback, error_callback) {
         url = APIHOST + url;
-
-        var responder = function(data) {
-            // Just in case...
-            return callback(data);
-        };
 
         return CacheService.get({
               url: url,
               data: params,
               dataType: 'json'
             })
-            .done(responder)
+            .done(function(data) {
+                // Just in case...
+                success_callback(data);
+            })
             .fail(function(req, status, error) {
+              if (error_callback) {
+                error_callback(req, status, error);
+              }
               if (!window.console) return;
               console.log('Failed to load: ', url);
               console.log(error);
