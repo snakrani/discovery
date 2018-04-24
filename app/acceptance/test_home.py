e45b5bde7e8157179ddf8fe0d6eb1ee42b1d0b73
Adrian Webb <adrian@webb.sh>
2018-04-24 03:15:17 +0000

    Updates to the home page acceptance tests.

--- app/acceptance/test_home.py
+++ app/acceptance/test_home.py
@@ -13,8 +13,13 @@ class HomeTest(case.AcceptanceTestCase, metaclass = case.MetaAcceptanceSchema):
             'placeholder': 'enabled',
             'css:.se_filter': 'disabled'
         },
+        'load_dates': {
+            'wait': ('sec', 1),
+            'data_source_date_sam': ('text__matches', r'^[\d]*/[\d]*/[\d]*$'),
+            'data_source_date_fpds': ('text__matches', r'^[\d]*/[\d]*/[\d]*$')
+        },
         'footer': {
-            'link_text:OASIS Program Home': ('link', 'http://www.gsa.gov/oasis'),
-            'link_text:Check out our code on GitHub': ('link', 'https://github.com/PSHCDevOps/discovery')
+            'link_text:OASIS Program Home': ('link__equal', 'http://www.gsa.gov/oasis'),
+            'link_text:Check out our code on GitHub': ('link__equal', 'https://github.com/PSHCDevOps/discovery')
         } 
     }
