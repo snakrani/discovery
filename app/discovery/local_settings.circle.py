cf3bdc73857aa5faa31c01d2a52c5f5cebd149b7
Adrian Webb <adrian@webb.sh>
2018-04-25 15:57:42 +0000

    Adding Cloud.goc Django UAA auth configurations in the Discovery CircleCI settings.

--- app/discovery/local_settings.circle.py
+++ app/discovery/local_settings.circle.py
@@ -1,12 +1,15 @@
 
+#-------------------------------------------------------------------------------
+# Core Django settings
+
 #
-# General project variables
+# Debugging
 #
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 #
-# Database connections
+# Database configurations
 #
 DATABASES = {
     'default': {
@@ -18,7 +21,18 @@ DATABASES = {
     }
 }
 
+#-------------------------------------------------------------------------------
+# Django Addons
+
 #
 # REST configuration 
 #
 REST_API_TEST = True
+
+#
+# Cloud.gov UAA authentication
+#
+UAA_CLIENT_ID = config_value('UAA_CLIENT_ID', 'notused')
+UAA_CLIENT_SECRET = config_value('UAA_CLIENT_SECRET', 'notused')
+UAA_AUTH_URL = config_value('UAA_AUTH_URL', 'fake:')
+UAA_TOKEN_URL = config_value('UAA_TOKEN_URL', 'fake:')
