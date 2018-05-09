21245fb53df7b4360f2ab66ba4ce4d1c11d786c7
Adrian Webb <adrian@webb.sh>
2018-05-09 14:51:28 +0000

    Cleaning up the load_categories command.

--- app/categories/management/commands/load_categories.py
+++ app/categories/management/commands/load_categories.py
@@ -1,53 +1,26 @@
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.management import call_command
-
-import os
-import sys
-import logging
-import traceback
-import warnings
-
-
-warnings.filterwarnings('ignore')
-
-
-def vendor_logger():
-    return logging.getLogger('vendor')
-
-def display_error(info):
-    print("MAJOR ERROR -- PROCESS ENDING EXCEPTION --  {}".format(info))
-    traceback.print_tb(sys.exc_info()[2])
-    vendor_logger().debug("MAJOR ERROR -- PROCESS ENDING EXCEPTION -- {}".format(info))
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("-------BEGIN LOAD_CATEGORIES PROCESS-------")
+        print('> Loading keywords')
+        call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/keywords.json'))
         
-        try:
-            print('> Loading keywords')
-            call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/keywords.json'))
-            
-            print('> Loading NAICS codes')
-            call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/naics.json'))
-            
-            print('> Loading PSC codes')
-            call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/psc.json'))
-            
-            print('> Loading vendor setaside categories')
-            call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/setasides.json'))
-            
-            print('> Loading vendor pools')
-            call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/pools.json'))
-            
-            print('> Loading zones')
-            call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/zones.json'))
-            call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/zonestates.json'))          
-
-        except Exception as e:
-            display_error(e)
-            raise
-
-        print("-------END LOAD_CATEGORIES PROCESS-------")
+        print('> Loading NAICS codes')
+        call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/naics.json'))
+        
+        print('> Loading PSC codes')
+        call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/psc.json'))
+        
+        print('> Loading vendor setasides')
+        call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/setasides.json'))
+        
+        print('> Loading vendor pools')
+        call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/pools.json'))
+        
+        print('> Loading zones')
+        call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/zones.json'))
+        call_command('loaddata', "{}/{}".format(settings.BASE_DIR, 'categories/fixtures/zonestates.json'))          
