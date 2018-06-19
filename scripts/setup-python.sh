cef866f4968fc9de02fc77359e28c129dc0fd3a8
Adrian Webb <adrian@webb.sh>
2018-06-19 09:33:41 +0000

    Refactoring the setup-python script to eliminate the unneeded virtual environment.

--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -12,8 +12,6 @@ then
   rm -f "$LOG_FILE"
 fi
 
-VENV_DIR="/venv"
-
 echo "> Installing Python and CLI utilities" | tee -a "$LOG_FILE"
 apt-get update >>"$LOG_FILE" 2>&1
 apt-get install -y make gcc libdpkg-perl libpq-dev curl git ssh vim python3-dev python3-venv >>"$LOG_FILE" 2>&1
@@ -21,26 +19,17 @@ rm -rf /var/lib/apt/lists/* >>"$LOG_FILE" 2>&1
 
 curl -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py >>"$LOG_FILE" 2>&1
 python3 /tmp/get-pip.py --force-reinstall >>"$LOG_FILE" 2>&1
-
-#create virtual environment if it does not exist and activate
-if [ ! -d "$VENV_DIR" ]
-then
-  echo "> Creating a Python project virtual environment" | tee -a "$LOG_FILE"
-  python3 -m venv "$VENV_DIR" --without-pip >>"$LOG_FILE" 2>&1
-fi
-  
+ 
 if [ -f requirements.txt ]
 then
-  cp requirements.txt "$VENV_DIR/requirements.txt" >>"$LOG_FILE" 2>&1
+  cp requirements.txt "/tmp/requirements.txt" >>"$LOG_FILE" 2>&1
 fi
 if [ -f requirements-dev.txt ]
 then
-  cp requirements-dev.txt "$VENV_DIR/requirements-dev.txt" >>"$LOG_FILE" 2>&1
+  cp requirements-dev.txt "/tmp/requirements-dev.txt" >>"$LOG_FILE" 2>&1
 fi
-
-source "$VENV_DIR/bin/activate" >>"$LOG_FILE" 2>&1
 
 #install Python application requirements
 echo "> Installing Python project requirements" | tee -a "$LOG_FILE"
-pip3 install -r "$VENV_DIR/requirements.txt" >>"$LOG_FILE" 2>&1
-pip3 install -r "$VENV_DIR/requirements-dev.txt" >>"$LOG_FILE" 2>&1
+pip3 install -r "/tmp/requirements.txt" >>"$LOG_FILE" 2>&1
+pip3 install -r "/tmp/requirements-dev.txt" >>"$LOG_FILE" 2>&1
