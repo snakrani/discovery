34d5e518f3747df9cb95d5004816fc44602c75ed
Adrian Webb <adrian@webb.sh>
2018-06-25 15:03:31 +0000

    Fixing issues with the query and updating formatting of pool CSV download in the vendors views.

--- app/vendors/views.py
+++ app/vendors/views.py
@@ -48,30 +48,66 @@ def PoolCSV(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="search_results.csv"'
     writer = csv.writer(response)
-    #write results
-    vendors = Vendor.objects.all()
+        
+    # Vendor filtering
     setasides_all = SetAside.objects.all().order_by('far_order')
-    filter_text = []
-    #naics
+    vendors = Vendor.objects.all().distinct()
+    
     naics = Naics.objects.get(code=request.GET['naics-code'])
-    vehicle = request.GET['vehicle'].upper()
-    pool = Pool.objects.get(naics=naics, vehicle=vehicle)
-    vendors = vendors.filter(pools__pool=pool).distinct()
-    filter_text.append("NAICS code {0}".format(naics))
-
-    #setasides
+    vehicle = None
+    pools = []
+    zone = None
+    setasides = []
+    
+    if 'vehicle' in request.GET:
+        vehicle = request.GET['vehicle'].upper()
+        
+    if 'pool' in request.GET:
+        pools = [request.GET['pool'].upper()]
+        vendors = vendors.filter(pools__pool__id=pools[0])
+    else:
+        if vehicle:
+            pools = Pool.objects.filter(vehicle=vehicle)
+        else:
+            pools = Pool.objects.filter(naics=naics.code)
+            
+        vendors = vendors.filter(pools__pool__id__in=pools.values_list('id', flat=True))
+        
+    if 'zone' in request.GET:
+        zone = request.GET['zone']
+        vendors = vendors.filter(pools__zone__id=zone)
+    
     if 'setasides' in request.GET:
         setasides = request.GET.getlist('setasides')[0].split(',')
         setaside_objs = SetAside.objects.filter(code__in=setasides)
+        
         for sobj in setaside_objs:
             vendors = vendors.filter(pools__setasides=sobj)
 
-        filter_text.append("Set Aside filters: {0}".format(", ".join(setasides)))
-
-    writer.writerow(("Vehicle: " + vehicle,))
+    writer.writerow(('Discovery research results',))
+    writer.writerow(('',))
+    writer.writerow(('NAICS code', "{}: {}".format(naics.code, naics.description)))
+    
+    if zone:
+        writer.writerow(('Zone', zone))
+    
+    first_pool = True
+    for pool in pools:
+        name = "{} pool {}: {}".format(" ".join(pool.vehicle.split('_')), pool.number, pool.name)
+        
+        if first_pool:
+            writer.writerow(('Pools', name))
+            first_pool = False
+        else:
+            writer.writerow(('', name))
+    
+    if len(setasides):
+        writer.writerow(('Setasides', ", ".join(setasides)))
+    
+    writer.writerow(('',))
+    writer.writerow(('',))
     writer.writerow(("Search Results: {0} Vendors".format(len(vendors)),))
-    writer.writerow(filter_text)
-
+    
     writer.writerow(('',))
     header_row = ['Vendor', 'Location', 'No. of Contracts',]
     header_row.extend([sa_obj.name for sa_obj in setasides_all])
