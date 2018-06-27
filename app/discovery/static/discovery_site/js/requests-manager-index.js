f57deb6fb6457d81936d4e5ddcb8dd6edae0b079
Adrian Webb <adrian@webb.sh>
2018-06-27 15:47:17 +0000

    Updating the homepage date loading event subscription in the requests-manager-index JS script.

--- app/discovery/static/discovery_site/js/requests-manager-index.js
+++ app/discovery/static/discovery_site/js/requests-manager-index.js
@@ -1,12 +1,12 @@
 
+RequestsManager.initializers.index = function() {
+    EventManager.subscribe('poolUpdated', this.loadMetadata.bind(RequestsManager));
+};
+
 RequestsManager.loadMetadata = function() {
     var url = "/api/metadata/";
 
     RequestsManager.getAPIRequest(url, {}, function(data) {
         EventManager.publish('dataLoaded', data);
     });
-};
-
-RequestsManager.load = function() {
-    this.loadMetadata();
 };
