86bd55d1733ab46bca3f8dbe8282c2e778f31963
Adrian Webb <adrian@webb.sh>
2018-07-09 17:32:22 +0000

    Updating the layout-manager JS script to use data-manager functions and adding toggleZone event handling.

--- app/discovery/static/discovery_site/js/layout-manager-index.js
+++ app/discovery/static/discovery_site/js/layout-manager-index.js
@@ -1,13 +1,14 @@
 
 LayoutManager.initializers.index = function() {
     EventManager.subscribe('dataChanged', this.route.bind(LayoutManager));
+    EventManager.subscribe('dataChanged', this.toggleZones.bind(LayoutManager));
     EventManager.subscribe('loadPage', this.vehicleInfo.bind(LayoutManager));
 
     this.hideZones();
 };
 
 LayoutManager.route = function(data) {
-    var queryObject = RequestsManager.buildRequestQuery();
+    var queryObject = DataManager.buildRequestQuery();
 
     if ('naics' in queryObject || 'vehicle' in queryObject || 'pool' in queryObject) {
         this.loadPoolPage();
@@ -15,7 +16,7 @@ LayoutManager.route = function(data) {
 };
 
 LayoutManager.loadPoolPage = function() {
-    var qs = URLManager.getQueryString();
+    var qs = DataManager.getQueryString();
     window.location.href = '/results' + qs;
 };
 
@@ -25,8 +26,8 @@ LayoutManager.render = function(results) {
             return ((dateObj.getMonth() + 1) + '/' + dateObj.getDate() + '/' + dateObj.getFullYear().toString().substring(2));
         };
 
-        $("#data_source_date_sam").text(LayoutManager.convertDate(results['sam_load_date']));
-        $("#data_source_date_fpds").text(LayoutManager.convertDate(results['fpds_load_date']));
+        $("#data_source_date_sam").text(this.convertDate(results['sam_load_date']));
+        $("#data_source_date_fpds").text(this.convertDate(results['fpds_load_date']));
     }
 };
 
