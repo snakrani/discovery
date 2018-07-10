c3ef116c427e289a52eff01ffc725436ab56cb1b
Adrian Webb <adrian@webb.sh>
2018-07-10 20:35:53 +0000

    Adding a Celery task definition for the prune_contracts command.

--- app/contracts/tasks.py
+++ app/contracts/tasks.py
@@ -13,7 +13,7 @@ import io
 
 
 @shared_task
-def update_contracts(period=520, load=520, count=500, pause=1):
+def update_contracts(period=260, load=260, count=500, pause=1):
     success = True
     lock_id = 'contracts.update_contracts'
     
@@ -55,6 +55,48 @@ def update_contracts(period=520, load=520, count=500, pause=1):
                    "load": load,
                    "count": count, 
                    "pause": pause
+        },
+        "message": mystdout.getvalue() 
+    }
+
+
+@shared_task
+def prune_contracts(period=260):
+    success = True
+    lock_id = 'contracts.prune_contracts'
+    
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = io.StringIO()
+    
+    try:
+        with db_mutex(lock_id):
+            # Commands don't return anything
+            call_command('prune_contracts', 
+                 period=period
+            )
+    
+    except DBMutexError:
+        success = False
+        print('prune_contracts: Could not obtain lock')
+        
+    except DBMutexTimeoutError:
+        print('prune_contracts: Task completed but the lock timed out')
+        
+    except Exception as error:
+        print(error)
+        
+        DBMutex.objects.filter(lock_id=lock_id).delete()
+        success = False
+    
+    sys.stdout = old_stdout
+    
+    if not success:
+        raise TaskError(mystdout.getvalue())
+        
+    return { 
+        "task": "prune_contracts",
+        "params": { 
+                   "period": period
         },
         "message": mystdout.getvalue() 
     }
