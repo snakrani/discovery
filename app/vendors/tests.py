2ec5d76a8507f75d8b4cf0baaee434c81bf6d5ee
Adrian Webb <adrian@webb.sh>
2018-07-11 04:29:37 +0000

    Removing the vendor capability statement unit tests from the vendor specific tests.

--- app/vendors/tests.py
+++ app/vendors/tests.py
@@ -5,14 +5,6 @@ from django.test import RequestFactory
 from django.core.management import call_command
 
 from vendors.models import Vendor
-from vendors.views import VendorView
-
-
-def make_view(view, request, *args, **kwargs):
-    view.request = request
-    view.args = args
-    view.kwargs = kwargs
-    return view
 
 
 class VendorLoadTest(case.BaseTestCase):
@@ -38,21 +30,3 @@ class VendorLoadTest(case.BaseTestCase):
     def test_pm_not_null(self):
         for vendor in Vendor.objects.all():
             self.assertNotEqual(vendor.managers.filter(type='PM').first().phones().count(), 0)
-
-
-class VendorViewTest(case.BaseTestCase):
-    def test_has_capability_statement_false(self):
-        request = RequestFactory().get('/vendor/0000?vehicle=oasis_sb')
-        view = VendorView(template_name='vendor.html')
-        view = make_view(view, request)
-        context = view.get_context_data(vendor_duns='0000')
-        self.assertFalse(context['has_capability_statement'])
-
-    def test_has_capability_statement(self):
-        request = RequestFactory().get('/vendor/626979228?vehicle=oasis_sb')
-        view = VendorView(template_name='vendor.html')
-        view = make_view(view, request)
-        context = view.get_context_data(vendor_duns='626979228')
-        self.assertTrue(context['has_capability_statement'])
-        self.assertEqual(context['capability_statement_url'],
-                         'discovery_site/capability_statements/oasis_sb/626979228.pdf')
