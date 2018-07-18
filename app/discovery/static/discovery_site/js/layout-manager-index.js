e3707f29f150045332fdd2a1905a8932d876f59d
Adrian Webb <adrian@webb.sh>
2018-07-18 11:19:45 +0000

    Refactoring and cleaning the layout-manager-index JS script.

--- app/discovery/static/discovery_site/js/layout-manager-index.js
+++ app/discovery/static/discovery_site/js/layout-manager-index.js
@@ -1,38 +1,22 @@
 
 LayoutManager.initializers.index = function() {
-    EventManager.subscribe('dataChanged', LayoutManager.route);
-    EventManager.subscribe('dataChanged', LayoutManager.toggleFilters);
-    EventManager.subscribe('dataChanged', LayoutManager.toggleZones);
-    EventManager.subscribe('pageInitialized', LayoutManager.vehicleInfo);
+    LayoutManager.initSearch();
 
-    LayoutManager.hideZones();
+    // Internal event subscriptions
+    EventManager.subscribe('pageInitialized', LayoutManager.styleVehicleInfo);
+    EventManager.subscribe('metadataLoaded', LayoutManager.renderMetadata);
 };
 
-LayoutManager.route = function(data) {
+LayoutManager.route = function() {
     var queryObject = DataManager.buildRequestQuery();
 
-    if ('naics' in queryObject || 'vehicle' in queryObject || 'pool' in queryObject) {
-        LayoutManager.loadPoolPage();
+    if ('naics' in queryObject || 'vehicle' in queryObject || 'pool' in queryObject || 'setasides' in queryObject) {
+        var qs = DataManager.getQueryString();
+        window.location.href = '/results' + qs;
     }
 };
 
-LayoutManager.loadPoolPage = function() {
-    var qs = DataManager.getQueryString();
-    window.location.href = '/results' + qs;
-};
-
-LayoutManager.render = function(results) {
-    if (! $.isEmptyObject(results)) {
-        var dateStr = function(dateObj) {
-            return ((dateObj.getMonth() + 1) + '/' + dateObj.getDate() + '/' + dateObj.getFullYear().toString().substring(2));
-        };
-
-        $("#data_source_date_sam").text(LayoutManager.convertDate(results['sam_load_date']));
-        $("#data_source_date_fpds").text(LayoutManager.convertDate(results['fpds_load_date']));
-    }
-};
-
-LayoutManager.vehicleInfo = function() {
+LayoutManager.styleVehicleInfo = function() {
     $('#discovery_vehicles').collapsible({
         accordion: false,
         accordionUpSpeed: 100,
@@ -43,4 +27,12 @@ LayoutManager.vehicleInfo = function() {
         arrowDclass: 'arrow-d',
         animate: true
     });
+};
+
+LayoutManager.renderMetadata = function(data) {
+    if (! $.isEmptyObject(data)) {
+        $("#data_source_date_sam").text(Format.convertDate(data['sam_load_date']));
+        $("#data_source_date_fpds").text(Format.convertDate(data['fpds_load_date']));
+    }
+    DataManager.completeStatus();
 };
