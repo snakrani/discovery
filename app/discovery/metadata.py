4bb78de60a9932be5b5ded067d7999f143144549
Adrian Webb <adrian@webb.sh>
2018-08-06 14:29:02 +0000

    Updating the SamLoad time field in the DiscoveryMetadata class definition.

--- app/discovery/metadata.py
+++ app/discovery/metadata.py
@@ -7,11 +7,11 @@ from contracts import models as contracts
 class DiscoveryMetadata(object):
     
     def __init__(self):
-        sam_md = vendors.SamLoad.objects.all().order_by('-load_date')
+        sam_md = vendors.SamLoad.objects.all().order_by('-load_time')
         fpds_md = contracts.FPDSLoad.objects.all().order_by('-load_date')
         
         if len(sam_md) > 0:
-            self.sam_load_date = sam_md[0].load_date
+            self.sam_load_date = sam_md[0].load_time.date()
         else: 
             self.sam_load_date = None
         if len(fpds_md) > 0:
