3411b192aa1a9b53b33841fcbb7d398086e84fa0
Adrian Webb <adrian@webb.sh>
2018-09-11 21:08:38 +0000

    Updating the dump_categories command with tier data dumping.

--- app/categories/management/commands/dump_categories.py
+++ app/categories/management/commands/dump_categories.py
@@ -22,6 +22,7 @@ class Command(BaseCommand):
         call_command('dumpdata', 'categories.setaside', indent=2, output="{}/{}".format(settings.BASE_DIR, 'categories/fixtures/setasides.json'))
         
         print('> Dumping vendor vehicles')
+        call_command('dumpdata', 'categories.tier', indent=2, output="{}/{}".format(settings.BASE_DIR, 'categories/fixtures/tiers.json'))
         call_command('dumpdata', 'categories.vehicle', indent=2, output="{}/{}".format(settings.BASE_DIR, 'categories/fixtures/vehicles.json'))
         
         print('> Dumping vendor pools')
