b73081643f90d2d90766b3916db7d91ebab0e194
Adrian Webb <adrian@webb.sh>
2018-09-11 20:13:47 +0000

    Adding keyword id filtering to the PSC API tests.

--- app/api/tests/test_psc.py
+++ app/api/tests/test_psc.py
@@ -107,14 +107,24 @@ class PscTest(case.APITestCase, metaclass = case.MetaAPISchema):
                 '@regex': '[A-Z]\d+\-\d+$',
                 '@iregex': '^(C87|51)'
             },
-            'naics__keywords__name': {
-                'tags': ('psc_field', 'naics_field', 'keyword_field', 'fuzzy_text'),
-                '@exact': 'Cooking Equipment',
+            'keywords__id': {
+                'tags': ('psc_field', 'keyword_field', 'number'),
+                '@exact': 66,
+                '@lt': 500,
+                '@lte': 500, 
+                '@gt': 250, 
+                '@gte': 250,
+                '@range': (50, 100),
+                '@in': (7, 450, 916)
+            },
+            'keywords__name': {
+                'tags': ('psc_field', 'keyword_field', 'fuzzy_text'),
+                '@exact': 'Utility Trucks, Platform Trucks, Handtrucks And Mail Carts',
                 '@iexact': 'ancillary supplies and / or services',
                 '@in': ("Elemental Analyzers", "Energy Consulting Services", "Environmental Consulting Services"),
                 '@contains': 'Support',
                 '@icontains': 'support',
-                '@startswith': 'Marine',
+                '@startswith': 'Plumbing',
                 '@istartswith': 'edu',
                 '@endswith': 'Services',
                 '@iendswith': 'services',
