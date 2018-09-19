7d8762511b11c5849ac2cb242d272ba0f935d793
Adrian Webb <adrian@webb.sh>
2018-09-19 16:42:37 +0200

    Adding Angular paths to the Discovery Django url configuration.

--- app/discovery/urls.py
+++ app/discovery/urls.py
@@ -19,8 +19,20 @@ if settings.UAA_AUTH:
 
 urlpatterns.extend([
     url(r'^admin/', admin.site.urls),
+
     url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^about/', TemplateView.as_view(template_name='index.html')),
+    url(r'^resources/', TemplateView.as_view(template_name='index.html')),
+    url(r'^help/', TemplateView.as_view(template_name='index.html')),
+
+    url(r'^search/', TemplateView.as_view(template_name='index.html')),
+    url(r'^vendor/', TemplateView.as_view(template_name='index.html')),
     
+    url(r'^oasis/', TemplateView.as_view(template_name='index.html')),
+    url(r'^bmo/', TemplateView.as_view(template_name='index.html')),
+    url(r'^hcats/', TemplateView.as_view(template_name='index.html')),
+    url(r'^pss/', TemplateView.as_view(template_name='index.html')),
+        
     url(r'^api/', include('api.urls')),
     url(r'^api/', include_docs_urls(title="Discovery API", public=True)),
     url(r'^docs/', RedirectView.as_view(url='/api', permanent=False)),
