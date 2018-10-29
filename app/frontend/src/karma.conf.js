71de5ba48cec128306c61ffb59cb5df107896c28
Adrian Webb <adrian@webb.sh>
2018-10-29 07:19:35 +0000

    Updates to the Angular Karma test configuration.

--- app/frontend/src/karma.conf.js
+++ app/frontend/src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     concurrency: 1,
     autoWatch: false,
     browsers: ['DiscoveryChrome'],
