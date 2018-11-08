b0bcd4d884faf126a0009291434f9499cbfe9e65
Adrian Webb <adrian@webb.sh>
2018-11-08 04:25:20 -0500

    Adding Google Analytics tracking id context processor.

--- app/discovery/context_processors.py
+++ app/discovery/context_processors.py
@@ -1,5 +1,12 @@
 from django.conf import settings
 
-def api_host(request):
-    return { "API_HOST": settings.API_HOST }
 
+def api_host(request):
+    return { 
+        "API_HOST": settings.API_HOST
+    }
+
+def google_analytics(request):
+    return { 
+        "GA_TRACKING_ID": settings.GA_TRACKING_ID 
+    }
