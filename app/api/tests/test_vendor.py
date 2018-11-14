c2a2fb55f5c0ba37264b852365cad645924f08b9
Adrian Webb <adrian@webb.sh>
2018-11-14 03:02:32 +0000

    Adding test for membership query in the API test vendor library.

--- app/api/tests/test_vendor.py
+++ app/api/tests/test_vendor.py
@@ -518,6 +518,14 @@ class VendorTest(case.APITestCase, metaclass = case.MetaAPISchema):
                         ('pools__pool__vehicle__id', 'exact', 'PSS'),
                         ('pools__setasides__code', 'in', ('A6', 'XX')),
                     )
+                },
+                '-membership2': {
+                    'tags': ('vendor_request',),
+                    'params': {'membership': '(pool__vehicle__id=BMO)&(setasides__code=A6)&(setasides__code=XX)'},
+                    'tests': (
+                        ('pools__pool__vehicle__id', 'exact', 'BMO'),
+                        ('pools__setasides__code', 'in', ('A6', 'XX')),
+                    )
                 }
             }
         }
