f289f99582d2d18e6d672ad6f7a0714a71c51d14
Adrian Webb <adrian@webb.sh>
2018-11-17 05:52:13 +0000

    Adding a cache utility library with views caching functionality.

---
+++ app/discovery/cache.py
@@ -0,0 +1,33 @@
+from collections import OrderedDict
+
+from django.conf import settings
+from django.core.cache import cache
+
+from rest_framework.response import Response
+
+from discovery import models as system
+
+import re
+import json
+
+
+def page_cache_key(request):
+    return "{}:{}".format(request.path, re.sub(r'[\s\{\}\(\)\"\=]', '', json.dumps(OrderedDict(request.query_params))))
+
+def track_page_load(url):
+    page, created = system.CachePage.objects.get_or_create(url=url)
+    page.count += 1
+    page.save()
+
+def cached_response(request, view_op):
+    page_id = page_cache_key(request)
+        
+    track_page_load(request.build_absolute_uri())
+    data = cache.get(page_id)
+        
+    if data:
+        return Response(data)
+    else:
+        response = view_op()
+        cache.set(page_id, response.data)
+        return response
